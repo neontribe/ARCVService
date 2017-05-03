e710a7420f95316c322996e977ea11d0f8ce07d8
katjam <katja@neontribe.co.uk>
2017-05-03 19:05:07 +0100

    Update seeders

--- database/seeds/SponsorsSeeder.php
+++ database/seeds/SponsorsSeeder.php
@@ -11,14 +11,13 @@ class SponsorsSeeder extends Seeder
      */
     public function run()
     {
-        $sponsors = [];
         $sponsorData = [
             ['name' => "imperium.gov", "shortcode" =>"SOL"],
             ['name' => "CthulhuShire County Council", 'shortcode' => "CCC"],
         ];
 
         foreach ($sponsorData as $sponsor) {
-            $sponsors[] = factory(App\Sponsor::class)->create($sponsor);
+            factory(App\Sponsor::class)->create($sponsor);
         }
     }
 }
