e9ef1d536e6afc46fc6360a836f20e5635646814
katjam <katja@neontribe.co.uk>
2017-05-05 09:39:44 +0100

    Add files for Big Route Reshuffle

---
+++ app/Http/Controllers/Service/TraderController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Service;
+
+use DB;
+use App\Trader;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TraderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Trader  $trader
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trader $trader)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Trader  $trader
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Trader $trader)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Trader  $trader
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trader $trader)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Trader  $trader
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trader $trader)
+    {
+        //
+    }
+}
