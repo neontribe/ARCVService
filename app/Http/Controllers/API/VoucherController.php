d1cd6ee561f5dea0ac2ac6dd108cc2d14892d041
katjam <katja@neontribe.co.uk>
2017-05-05 12:19:29 +0100

    Starting tests

--- app/Http/Controllers/API/VoucherController.php
+++ app/Http/Controllers/API/VoucherController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers\API;
 
-use Illuminate\Pagination\Paginator;
-use Response;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Auth;
--- tests/Unit/Controllers/API/VoucherControllerTest.php
+++ tests/Unit/Controllers/API/VoucherControllerTest.php
@@ -14,35 +14,42 @@ class VoucherControllerTest extends TestCase
 
     use DatabaseMigrations, DatabaseTransactions;
 
-    protected $vouchers;
-
-    protected $payload = {
-        trader_id: 1,
-        user_id: 1,
-        vouchers: [
-            "notavoucher",
-            "UYH78787878",
-            "SOL00000001",
-            "SOL00000002",
-            "SOL00000003"
+    protected $vouchers, $user;
+    protected $payload = [
+        'trader_id' => 1,
+        'user_id' => 1,
+        'vouchers' => [
+            'notavoucher',
+            'UYH78787878',
+            'SOL00000001',
+            'SOL00000002',
+            'SOL00000003'
         ]
-    };
+    ];
 
     protected function setUp()
     {
         parent::setUp();
+
+        $this->user = factory(User::class)->create();
+
         // All belong to user1, trader 1.
         $codes = ['SOL00000001', 'SOL00000002', 'SOL00000003', 'SOL00000004'];
-        foreach ($codes as $code)
-        // Cannot be collected.
-        $this->vouchers[] = factory(Voucher::class)->create([
-            'code' => $code,
-        ];
+        foreach ($codes as $code) {
+            // Cannot be collected.
+            $this->vouchers[] = factory(Voucher::class)->create([
+                'code' => $code,
+            ]);
+        }
     }
 
     public function testVouchersCollect()
     {
-        //Todo
+        $this->actingAs($this->user)
+            ->json('POST', route('api.voucher.collect'), $this->payload);
+            // Not getting the type of response object I was expecting.
+            //->dump();
+            //->seeStatusCode(200);
     }
     public function testVouchersShow()
     {
