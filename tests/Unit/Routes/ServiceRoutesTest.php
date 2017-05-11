4f92503454e252b54c48539107c67dcad3a33884
katjam <katja@neontribe.co.uk>
2017-05-11 13:27:08 +0100

    Better Voucher controller and tests

--- tests/Unit/Routes/ServiceRoutesTest.php
+++ tests/Unit/Routes/ServiceRoutesTest.php
@@ -18,16 +18,22 @@ class ServiceRoutesTest extends TestCase
         $this->vouchers = factory(Voucher::class, 10)->create();
      }
 
-    public function testVouchersResourseRoutes()
+    public function testVouchersIndexRoute()
     {
         $this->get(route('vouchers.index'))
             ->assertStatus(200)
-        ;
-
-        $this->json('GET', route('vouchers.index'))
             ->assertJsonStructure([ 0 => [
                 'id', 'trader_id', 'code', 'currentstate', 'sponsor_id'
             ]])
         ;
     }
+
+    public function testVouchersShowRoute()
+    {
+        $this->get(route('vouchers.show', $this->vouchers[0]))
+            ->assertStatus(200)
+            ->assertJsonStructure(['id', 'trader_id', 'code', 'currentstate', 'sponsor_id'])
+        ;
+    }
+
 }
