7e4f08cb94013609255f6ccbc6982dd962f88e13
katjam <katja@neontribe.co.uk>
2017-06-19 11:03:11 +0100

    Fix conflict in email and add copy alert.

--- resources/views/api/emails/voucher_history_email.blade.php
+++ resources/views/api/emails/voucher_history_email.blade.php
@@ -1,19 +1,15 @@
 @extends('layouts.email')
 
 @section('content')
-
-<<<<<<< HEAD
-    <p>Voucher History Email</p>
-=======
+[xXx]
     <p>Hi there,</p>
 
     <p>You've requested a record of your voucher payment history, which is attached to this email.</p>
-
+    <!-- Is this the right email> -->
     <p>If you have any problems with opening or downloading the file attached, please <a href="mailto:arc@neontribe.co.uk">email arc@neontribe.co.uk</a>.</p>
 
     <p>Thanks,<br>
     Rose Vouchers<br>
     <a href="http://www.alexandrarose.org.uk/">www.alexandrarose.org.uk</a></p>
->>>>>>> 194d412dfb61fc48c2ec3c4709349c3576e2a119
-
+[xXx]
 @endsection
