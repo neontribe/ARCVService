121001bf33035e41ff1279ca58c1b8d86498ded5
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-06-22 11:15:42 +0100

    textonly email added and enabled

---
+++ resources/views/api/emails/voucher_history_email_textonly.blade.php
@@ -0,0 +1,19 @@
+* VOUCHER PAYMENT HISTORY REQUEST *
+
+==============================================
+
+Hi {{ $user }},
+
+You've requested a record of {{ $trader }}'s voucher payment history, which is attached to this email.
+
+If you have any problems with opening or downloading the file attached, please <a href="mailto:arc@neontribe.co.uk">email arc@neontribe.co.uk</a>.
+
+Thanks,
+Rose Vouchers
+<a href="http://www.alexandrarose.org.uk/">www.alexandrarose.org.uk</a>
+
+==============================================
+
+For more information please go to <a href="http://www.alexandrarose.org.uk/">www.alexandrarose.org.uk</a> or email us at <a href="mailto:arc@neontribe.co.uk">arc@neontribe.co.uk</a>.
+
+<a href="http://www.alexandrarose.org.uk/privacy/">Privacy Policy</a>
