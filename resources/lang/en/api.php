29ceaca5aacbc3df013bf0f9fda4c3f9dd8ad064
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-06-22 16:11:37 +0100

    removed xXx

--- resources/lang/en/api.php
+++ resources/lang/en/api.php
@@ -2,7 +2,7 @@
 
 return [
 	'messages'	=> [
-		'email_voucher_history' => '[xXx]Thanks. If you don\'t receive an email with your voucher history, please try again later.'
+		'email_voucher_history' => 'Thanks. If you don\'t receive an email with your voucher history, please try again later.'
 	],
 	'errors' => [
 		'invalid_credentials' => 'The user credentials were incorrect.'
