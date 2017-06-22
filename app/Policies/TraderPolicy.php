8204d35a74b509410931da36eb263b008ca3cf16
katjam <katja@neontribe.co.uk>
2017-06-22 17:22:58 +0100

    Set up middleware for service data

--- app/Policies/TraderPolicy.php
+++ app/Policies/TraderPolicy.php
@@ -11,13 +11,13 @@ class TraderPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view the trader.
+     * Determine whether the api user can view the trader.
      *
      * @param  \App\User  $user
      * @param  \App\Trader  $trader
      * @return mixed
      */
-    public function view(User $user, Trader $trader)
+    public function apiView(User $user, Trader $trader)
     {
         // Only if the Trader belongs to the user.
         return $user->hasTrader($trader);
--- routes/service.php
+++ routes/service.php
@@ -8,47 +8,48 @@ use Carbon\Carbon;
 |--------------------------------------------------------------------------
 | Service Routes
 |--------------------------------------------------------------------------
-*/
+ */
 
-// TODO: sort these out into proper PS-4 heirarchy or something...
+// For now these routes are only available in dev and staging environs.
+Route::group(['middleware' => 'isNotProduction'], function () {
+    Route::resource('vouchers', 'VoucherController', [
+        'only' => ['index','show','store']
+    ]);
 
-Route::resource('vouchers', 'VoucherController', [
-    'only' => ['index','show','store']
-]);
+    Route::resource('users', 'UserController', [
+        'only' => ['index','show',]
+    ]);
 
-Route::resource('users', 'UserController', [
-    'only' => ['index','show',]
-]);
+    Route::resource('markets', 'MarketController', [
+        'only' => ['index','show',]
+    ]);
 
-Route::resource('markets', 'MarketController', [
-    'only' => ['index','show',]
-]);
-
-Route::resource('traders', 'TraderController', [
-    'only' => ['index','show']
-]);
+    Route::resource('traders', 'TraderController', [
+        'only' => ['index','show']
+    ]);
 
 
-// Temp route for demo only.
-Route::get('reset-data', function () {
-    $process = new Process('
+    // Temp route for demo only.
+    Route::get('reset-data', function () {
+        $process = new Process('
         php ../artisan migrate:refresh --seed --force
     ');
-    $process->run();
-    $process = new Process('
+        $process->run();
+        $process = new Process('
         php ../artisan passport:install
     ');
-    $process->run();
+        $process->run();
 
-    $new_secret = DB::table('oauth_clients')->where('id', 2)->pluck('secret')[0];
-    $env_file_path = base_path('.env');
-    $old_secret = env('PASSWORD_CLIENT_SECRET');
-    file_put_contents($env_file_path, preg_replace(
+        $new_secret = DB::table('oauth_clients')->where('id', 2)->pluck('secret')[0];
+        $env_file_path = base_path('.env');
+        $old_secret = env('PASSWORD_CLIENT_SECRET');
+        file_put_contents($env_file_path, preg_replace(
         "/^PASSWORD_CLIENT_SECRET={$old_secret}/m",
         "PASSWORD_CLIENT_SECRET={$new_secret}",
         file_get_contents($env_file_path)
     ));
 
-    return Redirect::route('dashboard')
+        return Redirect::route('dashboard')
         ->with('message', 'Reseeded @' . Carbon::now());
+    });
 });
