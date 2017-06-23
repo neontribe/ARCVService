cc0909fe793e286a9b63fcfe738866ce65594ecd
katjam <katja@neontribe.co.uk>
2017-06-23 12:48:39 +0100

    Move policies and only display routes to staging and devs

--- app/Http/Middleware/IsNotProduction.php
+++ app/Http/Middleware/IsNotProduction.php
@@ -17,6 +17,10 @@ class IsNotProduction
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        if (config('app.url') === 'https://voucher-admin.alexandrarose.org.uk') {
+            // We should only see thes routes in dev and staging environs.
+            return response('Unauthorized', 418);
+        }
         return $next($request);
     }
 }
---
+++ app/Policies/Api/TraderPolicy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Policies\Api;
+
+use App\User;
+use App\Trader;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TraderPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the api user can view the trader.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Trader  $trader
+     * @return mixed
+     */
+    public function view(User $user, Trader $trader)
+    {
+        // Only if the Trader belongs to the user.
+        return $user->hasTrader($trader);
+    }
+
+    /**
+     * Determine whether the user can create traders.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+    }
+
+    /**
+     * Determine whether the user can update the trader.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Trader  $trader
+     * @return mixed
+     */
+    public function update(User $user, Trader $trader)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the trader.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Trader  $trader
+     * @return mixed
+     */
+    public function delete(User $user, Trader $trader)
+    {
+        //
+    }
+}
