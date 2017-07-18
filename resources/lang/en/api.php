29a858cafcb38500f047b1d0f36e2382eeed2bfd
katjam <katja@neontribe.co.uk>
2017-07-18 14:05:28 +0100

    Add message to payment requested.

--- resources/lang/en/api.php
+++ resources/lang/en/api.php
@@ -1,10 +1,13 @@
 <?php
 
 return [
-	'messages'	=> [
-		'email_voucher_history' => 'Thanks. If you don\'t receive an email with your voucher history, please try again later.'
-	],
-	'errors' => [
-		'invalid_credentials' => 'The user credentials were incorrect.'
-	]
+    'messages'    => [
+        'email_voucher_history' => 'Thanks. If you don\'t receive an email with your voucher history, please try again later.'
+    ],
+        'voucher_payment_requested' => '[xXx] Thanks. An email has been sent to let the team at Alexandra Rose that you have requested payment.'
+    ],
+
+    'errors' => [
+        'invalid_credentials' => 'The user credentials were incorrect.'
+    ]
 ];
