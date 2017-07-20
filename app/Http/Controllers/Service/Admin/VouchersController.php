6865619df1d8d4aaacd353becf2c3a52434f6287
katjam <katja@neontribe.co.uk>
2017-07-20 16:50:55 +0100

    Add batch of vouchers

--- app/Http/Controllers/Service/Admin/VouchersController.php
+++ app/Http/Controllers/Service/Admin/VouchersController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Service\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Sponsor;
 use App\Voucher;
 use Auth;
+use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Http\Request;
 use Log;
@@ -33,24 +35,32 @@ class VouchersController extends Controller
     {
         // A range of numbers from first to last.
         // Last can be empty. Then only one is created.
-        return view('service.vouchers.create');
+        $sponsors = Sponsor::all();
+        return view('service.vouchers.create', compact('sponsors'));
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store Voucher range.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store($vouchers)
+    public function storeBatch(Request $request)
     {
-        //$new_vouchers = [];
-        //foreach($vouchers as $voucher) {
-        //    $v = new Voucher();
-        //    $v-> = $voucher->;
-        //    $v-> = $voucher->;
-        //    $new_vouchers[] = $v->attributesToArray();
-        //}
-        //Voucher::insert($new_vouchers);
+        $new_vouchers = [];
+        $codes = range($request['start'], $request['end']);
+        $sponsor = Sponsor::find($request['sponsor_id'])->first();
+        foreach ($codes as $c) {
+            $v = new Voucher();
+            $v->code = $sponsor->shortcode . $c;
+            $v->sponsor_id = $request['sponsor_id'];
+            $v->currentstate = 'requested';
+            $v->created_at = Carbon::now();
+            $v->updated_at = Carbon::now();
+            $new_vouchers[] = $v->attributesToArray();
+        }
+        Voucher::insert($new_vouchers);
+
+        // Todo progress vouchers to allocated?
     }
 }
--- resources/views/service/vouchers/create.blade.php
+++ resources/views/service/vouchers/create.blade.php
@@ -1,1 +1,48 @@
-create
+@extends('service.layouts.app')
+
+@section('content')
+
+<h1>Add Vouchers</h2>
+
+     <form role="form" method="POST" action="{{ route('vouchers.storebatch') }}">
+            {!! csrf_field() !!}
+
+            <p>
+                <label for="sponsor_id" class="required">Sponsor</label>
+                <select name="sponsor_id" id="sponsor_id" class="{{ $errors->has('sponsor_id') ? 'has-error' : '' }}">
+                    <option value="">Please Select a Sponsor</option>
+                    @foreach ($sponsors as $sponsor)
+                    <option value="{{ $sponsor->id }}"
+                    >{{ $sponsor->name }}
+                    </option>
+                    @endforeach
+                </select>
+            </p>
+
+            @if ($errors->has('sponsor_id'))
+                <p class="error">{{ $errors->first('sponsor_id') }}</p>
+            @endif
+
+            <p>
+                <label for="start" class="required">Voucher Batch Start</label>
+                <input type="text" id="start" name="start" class="{{ $errors->has('start') ? 'error' : '' }}" required>
+            </p>
+
+            @if ($errors->has('start'))
+                <p class="error">{!! $errors->first('start') !!}</p>
+            @endif
+
+            <p>
+                <label for="current_password"></label>
+                <input type="text" id="end" name="end" class="{{ $errors->has('end') ? 'error' : '' }}">
+            </p>
+            @if ($errors->has('end'))
+                <p class="error">{{ $errors->first('end') }}</p>
+            @endif
+            <p>
+                <button type="submit">Create Vouchers</button>
+            </p>
+
+        </form>
+
+@endsection
