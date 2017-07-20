ead2ef7c3bb99a2e771994a6984cc82f8206d259
Steph Adams <steph@neontribe.co.uk>
2017-07-20 15:28:28 +0100

    Fix table indentation

--- resources/views/service/vouchers/index.blade.php
+++ resources/views/service/vouchers/index.blade.php
@@ -11,30 +11,30 @@
 
         <table class="table table-striped">
             <thead>
-              <tr>
-                <th>Voucher ID</th>
-                <th>Trader ID</th>
-                <th>Voucher code</th>
-                <th>Status</th>
-                <th>Sponsor ID</th>
-                <th>Created at</th>
-                <th>Updated at</th>
-                <th>Deleted at</th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach ($vouchers as $voucher)
-                  <tr>
-                    <th scope="row">{{ $voucher->id }}</th>
-                    <td>{{ $voucher->trader_id }}</td>
-                    <td>{{ $voucher->code }}</td>
-                    <td>{{ $voucher->currentstate }}</td>
-                    <td>{{ $voucher->sponsor_id }}</td>
-                    <td>{{ $voucher->created_at }}</td>
-                    <td>{{ $voucher->updated_at }}</td>
-                    <td>{{ $voucher->deleted_at }}</td>
-                  </tr>
-              @endforeach
+                <tr>
+                    <th>Voucher ID</th>
+                    <th>Trader ID</th>
+                    <th>Voucher code</th>
+                    <th>Status</th>
+                    <th>Sponsor ID</th>
+                    <th>Created at</th>
+                    <th>Updated at</th>
+                    <th>Deleted at</th>
+                </tr>
+              </thead>
+              <tbody>
+                  @foreach ($vouchers as $voucher)
+                      <tr>
+                          <th scope="row">{{ $voucher->id }}</th>
+                          <td>{{ $voucher->trader_id }}</td>
+                          <td>{{ $voucher->code }}</td>
+                          <td>{{ $voucher->currentstate }}</td>
+                          <td>{{ $voucher->sponsor_id }}</td>
+                          <td>{{ $voucher->created_at }}</td>
+                          <td>{{ $voucher->updated_at }}</td>
+                          <td>{{ $voucher->deleted_at }}</td>
+                      </tr>
+                  @endforeach
             </tbody>
         </table>
 
