7bcad9ea7fb8d084ee3c55a01eb9d82bc754e78b
Steph Adams <steph@neontribe.co.uk>
2017-07-20 14:19:54 +0100

    Put live voucher data in a table

--- resources/views/service/vouchers/index.blade.php
+++ resources/views/service/vouchers/index.blade.php
@@ -9,13 +9,42 @@
 
       <h1>View live vouchers</h1>
 
-      <ul>
+      <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Voucher ID</th>
+          <th>Trader ID</th>
+          <th>Voucher code</th>
+          <th>Status</th>
+          <th>Sponsor ID</th>
+          <th>Created at</th>
+          <th>Updated at</th>
+          <th>Deleted at</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($vouchers as $voucher)
+            <tr>
+              <th scope="row">{{ $voucher->id }}</th>
+              <td>{{ $voucher->trader_id }}</td>
+              <td>{{ $voucher->code }}</td>
+              <td>{{ $voucher->currentstate }}</td>
+              <td>{{ $voucher->sponsor_id }}</td>
+              <td>{{ $voucher->created_at }}</td>
+              <td>{{ $voucher->updated_at }}</td>
+              <td>{{ $voucher->deleted_at }}</td>
+            </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+      <!-- <ul>
       @foreach ($vouchers as $voucher)
 
       <li>{{ $voucher }}</li>
 
       @endforeach
-      </ul>
+      </ul> -->
 
     </div>
 
