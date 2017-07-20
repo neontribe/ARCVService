7c69ae23fa689f6717cf21a252f87c4762ad9eb7
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-07-20 16:43:40 +0100

    error and success messages moved from ARCVMarket

--- resources/lang/en/api.php
+++ resources/lang/en/api.php
@@ -6,9 +6,13 @@ return [
         'email_voucher_history' => 'Thanks. If you don\'t receive an email with your voucher history, please try again later.',
         'email_voucher_history_date' => '[xXx] Thanks, we\'ve received your request for payment history from :date. If you don\'t receive an email with your voucher history, please try again later.',
        'voucher_payment_requested' => '[xXx] Thanks. An email has been sent to let the team at Alexandra Rose that you have requested payment.',
+       'voucher_success' => '[xXx] Voucher is valid',
     ],
 
     'errors' => [
-        'invalid_credentials' => 'The user credentials were incorrect.'
+        'invalid_credentials' => 'The user credentials were incorrect.',
+        'invalid_voucher' => 'Please enter a valid voucher code.',
+        'own_dupe' => 'You have already submitted voucher code [xXx]',
+        'another_dupe' => 'It looks like this code has been used already, please double check and try again. If you are still unable to add the voucher code, don\'t worry - mark it as "unrecorded", send it in with your other vouchers and you will still be paid when we receive it. [xXx]',
     ]
 ];
