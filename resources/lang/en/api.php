b9e1769a964f31e3fa9fb6cac0facf63e6503cab
Steph Adams <steph@neontribe.co.uk>
2017-07-21 15:43:02 +0100

    Check copy

--- resources/lang/en/api.php
+++ resources/lang/en/api.php
@@ -3,9 +3,9 @@
 return [
 
     'messages' => [
-        'email_voucher_history' => 'Thanks. If you don\'t receive an email with your voucher history, please try again later.',
-        'email_voucher_history_date' => '[xXx] Thanks, we\'ve received your request for payment history from :date. If you don\'t receive an email with your voucher history, please try again later.',
-       'voucher_payment_requested' => '[xXx] Thanks. An email has been sent to let the team at Alexandra Rose that you have requested payment.',
+        'email_voucher_history' => 'Thanks. If you don\'t receive an email with your payment records, please try again later.',
+        'email_voucher_history_date' => 'Thanks, we\'ve received your request for payment records from :date. If you don\'t receive an email with your payment records, please try again later.',
+       'voucher_payment_requested' => '[xXx] Thanks. An email has been sent to the Alexandra Rose team to let them know that you have requested payment.',
     ],
 
     'errors' => [
