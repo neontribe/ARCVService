c5f0e982c45a231ecabe184f13f0418312f79335
katjam <katja@neontribe.co.uk>
2017-07-21 12:00:03 +0100

    Reset password controllers and routes for API

---
+++ app/Http/Controllers/API/Auth/ForgotPasswordController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Log;
+
+class ForgotPasswordController extends Controller
+{
+    use SendsPasswordResetEmails;
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return JSON Response
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validate($request, ['email' => 'required|email']);
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->broker()->sendResetLink(
+            $request->only('email')
+        );
+        if ($response === Password::RESET_LINK_SENT) {
+            Log::info('Rosie account reset password link email sent.');
+            return response()->json(['status', trans($response)]);
+        }
+        // If an error was returned by the password broker, we will get this message
+        // translated so we can notify a user of the problem. We'll redirect back
+        // to where the users came from so they can attempt this process again.
+        return response()->json(['email' => trans($response)], 422);
+    }
+}
---
+++ app/Http/Controllers/API/Auth/ResetPasswordController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Http\Request;
+
+class ResetPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
+
+    use ResetsPasswords;
+
+     /*
+     * Get the response for after a successful password reset.
+     *
+     * @param string $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function sendResetResponse($response)
+    {
+        return response()->json(['status' => trans($response)]);
+    }
+
+    /**
+     * Get the response for after a failing password reset.
+     *
+     * @param Request $request
+     * @param string $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function sendResetFailedResponse(Request $request, $response)
+    {
+        return response()->json(['email' => trans($response)], 422);
+    }
+}
