c2201f9a612e8293937cb4ab710c7182bbf06243
karalan <kara@neontribe.co.uk>
2018-07-30 15:19:30 +0000

    merging handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,13 @@
 
 namespace App\Exceptions;
 
+use Auth;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -40,10 +44,31 @@ class Handler extends ExceptionHandler
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Response|Response
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof TokenMismatchException) {
+            // Explicitly redirect to login form if token mismatches.
+            // Note: this will also draw attention to the login form timing out.
+            Auth::logout();
+            return redirect()
+                ->guest(route('service.login'))
+                // We anticipate expiry to be the most common reason.
+                ->withErrors(['error_message' => trans('auth.expired')])
+                ;
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            // User somehow tried an HTTP verb a resource doesn't support
+            // Send them somewhere safe.
+            $route = (Auth::check()) ? 'service.dashboard' : 'service.login';
+            return redirect()
+                // For some reason ->withErrors() won't "take";
+                // The session gets flashed, but it doesn't survive.
+                ->route($route);
+        }
+
         return parent::render($request, $exception);
     }
 
@@ -60,10 +85,15 @@ class Handler extends ExceptionHandler
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
         // For later if we have different users.
+
         $guard = array_get($exception->guards(), 0);
         switch ($guard) {
             case 'api':
                 $login = route('api.login');
+                break;
+            //TODO: merge
+            case 'store':
+                $login = route('store.login');
                 break;
             case 'admin':
             default:
@@ -71,6 +101,7 @@ class Handler extends ExceptionHandler
                 break;
         }
 
-        return redirect()->guest($login);
+        return redirect()->guest($login)
+            ->withErrors(['error_message' => trans('store.auth.exception')]);
     }
 }
