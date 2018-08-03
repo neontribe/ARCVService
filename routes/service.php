4269c4485a9d840b2969bc8b1ad5f62be08009b5
Charles Strange <charles@neontribe.co.uk>
2018-08-03 11:23:35 +0100

    move web.php routes to service.php

--- routes/service.php
+++ routes/service.php
@@ -10,6 +10,27 @@ use Carbon\Carbon;
 |--------------------------------------------------------------------------
 */
 
+// Admin (Service) Authentication Routes...
+Route::get('login', [
+    'as' => 'admin.login',
+    'uses' => 'Service\Auth\LoginController@showLoginForm',
+]);
+Route::post('login', 'Service\Auth\LoginController@login');
+
+Route::get('/', 'Service\AdminController@index')->name('admin.dashboard');
+
+// Admin (Service) Password Reset Routes...
+Route::get('password/reset', 'Service\Auth\ForgotPasswordController@showLinkRequestForm')
+    ->name('admin.password.request')
+;
+Route::post('password/email', 'Service\Auth\ForgotPasswordController@sendResetLinkEmail')
+    ->name('admin.password.email')
+;
+Route::get('password/reset/{token}', 'Service\Auth\ResetPasswordController@showResetForm')
+    ->name('admin.password.reset')
+;
+Route::post('admin.password/reset', 'Service\Auth\ResetPasswordController@reset');
+
 Route::group(['middleware' => 'auth:admin'], function () {
     Route::get('vouchers', [
         'as' =>'admin.vouchers.index',
