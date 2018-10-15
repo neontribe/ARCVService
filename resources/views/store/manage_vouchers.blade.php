a10d3afe8924cd38d058b895c552cf582901e2c7
alexalala <alexmoore94@hotmail.co.uk>
2018-10-15 12:16:04 +0100

    commented out form removed

--- resources/views/store/manage_vouchers.blade.php
+++ resources/views/store/manage_vouchers.blade.php
@@ -7,175 +7,173 @@
     @include('store.partials.navbar', ['headerTitle' => 'Voucher Manager'])
 
     <div class="content">
-        <!-- <form> -->
-            <div class="col">
+        <div class="col">
+            <div>
+                <img src="{{ asset('store/assets/info-light.svg') }}">
+                <h2>This Family</h2>
+            </div>
+            <div class="alongside-container">
                 <div>
-                    <img src="{{ asset('store/assets/info-light.svg') }}">
-                    <h2>This Family</h2>
-                </div>
-                <div class="alongside-container">
-                    <div>
 
-                        <h3>Main Carer:</h3>
-                        <p>{{ $pri_carer->name }}</p>
-                    </div>
-                    <div>
-                        <h3>Children:</h3>
-                        <ul>
-                          @foreach ( $children as $child )
-                            <li>{{ $child->getAgeString() }}</li>
-                          @endforeach
-                        </ul>
-                    </div>
+                    <h3>Main Carer:</h3>
+                    <p>{{ $pri_carer->name }}</p>
                 </div>
-                <div class="warning">
-                    @foreach( $family->getNoticeReasons() as $notices )
-                    <p class="v-spaced">
-                        <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
-                        Warning: {{ $notices['count'] }} {{ str_plural($notices['entity'], $notices['count']) }}
-                        is {{ $notices['reason'] }}
-                    </p>
-                    @endforeach
+                <div>
+                    <h3>Children:</h3>
+                    <ul>
+                      @foreach ( $children as $child )
+                        <li>{{ $child->getAgeString() }}</li>
+                      @endforeach
+                    </ul>
                 </div>
-                <button class="short-button" onclick="javascript:window.location.href='{{ URL::route("store.registration.edit", ['id' => $registration->id ]) }}'; return false;">
-                  Go to edit family
+            </div>
+            <div class="warning">
+                @foreach( $family->getNoticeReasons() as $notices )
+                <p class="v-spaced">
+                    <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+                    Warning: {{ $notices['count'] }} {{ str_plural($notices['entity'], $notices['count']) }}
+                    is {{ $notices['reason'] }}
+                </p>
+                @endforeach
+            </div>
+            <button class="short-button" onclick="javascript:window.location.href='{{ URL::route("store.registration.edit", ['id' => $registration->id ]) }}'; return false;">
+              Go to edit family
+            </button>
+            <button class="long-button">Find another family</button>
+        </div>
+        <div class="col">
+            <div>
+                <img src="{{ asset('store/assets/history-light.svg') }}">
+                <h2>Collection History</h2>
+            </div>
+            <div>
+                <p>This family should collect {{ $family->entitlement }} vouchers per week</p>
+                <p class="v-spaced">Their last collection was on Thursday 2nd August</p>
+            </div>
+            <div class="center">
+                <span id="brief-toggle" class="show clickable-span">
+                  brief collection history
+                  <i class="fa fa-caret-down" aria-hidden="true"></i>
+                </span>
+                <div id="brief" class="collapsed">
+                    <table>
+                        <tr>
+                            <th>W/C</th>
+                            <th>Vouchers collected</th>
+                        </tr>
+                        <tr>
+                            <td>17/09/2018</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>10/09/2018</td>
+                            <td>0</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <button class="long-button">Full collection history</button>
+        </div>
+        <div class="col allocation">
+            <div>
+                <img src="{{ asset('store/assets/allocation-light.svg') }}">
+                <h2>Allocate Vouchers</h2>
+            </div>
+            <div class="alongside-container">
+                <label>
+                    First voucher
+                    <input type="text">
+                </label>
+                <label>
+                    Last voucher
+                    <input id="last-voucher" type="text">
+                </label>
+                <button class="add-button">
+                    <i class="fa fa-plus" aria-hidden="true"></i>
                 </button>
-                <button class="long-button">Find another family</button>
             </div>
-            <div class="col">
-                <div>
-                    <img src="{{ asset('store/assets/history-light.svg') }}">
-                    <h2>Collection History</h2>
-                </div>
-                <div>
-                    <p>This family should collect {{ $family->entitlement }} vouchers per week</p>
-                    <p class="v-spaced">Their last collection was on Thursday 2nd August</p>
-                </div>
-                <div class="center">
-                    <span id="brief-toggle" class="show clickable-span">
-                      brief collection history
-                      <i class="fa fa-caret-down" aria-hidden="true"></i>
-                    </span>
-                    <div id="brief" class="collapsed">
-                        <table>
-                            <tr>
-                                <th>W/C</th>
-                                <th>Vouchers collected</th>
-                            </tr>
-                            <tr>
-                                <td>17/09/2018</td>
-                                <td>0</td>
-                            </tr>
-                            <tr>
-                                <td>10/09/2018</td>
-                                <td>0</td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-                <button class="long-button">Full collection history</button>
-            </div>
-            <div class="col allocation">
-                <div>
-                    <img src="{{ asset('store/assets/allocation-light.svg') }}">
-                    <h2>Allocate Vouchers</h2>
-                </div>
-                <div class="alongside-container">
-                    <label>
-                        First voucher
-                        <input type="text">
-                    </label>
-                    <label>
-                        Last voucher
-                        <input id="last-voucher" type="text">
-                    </label>
-                    <button class="add-button">
+            <p class="center">OR</p>
+            <div>
+                <label for="add-voucher-input">Add individual vouchers</label>
+                <div class="small-button-container">
+                    <input type="text" id="add-voucher-input">
+                    <button class="addButton">
                         <i class="fa fa-plus" aria-hidden="true"></i>
                     </button>
                 </div>
-                <p class="center">OR</p>
-                <div>
-                    <label for="add-voucher-input">Add individual vouchers</label>
-                    <div class="small-button-container">
-                        <input type="text" id="add-voucher-input">
-                        <button class="addButton">
-                            <i class="fa fa-plus" aria-hidden="true"></i>
-                        </button>
-                    </div>
-                </div>
-                <p class="center vh-spaced">You have added {{ $vouchers_amount }} vouchers</p>
-                <button id="collection-button" class="long-button">Go to voucher collection</button>
-                <div class="center">
-                    <span class="clickable-span">
-                        Vouchers added
-                        <i class="fa fa-caret-down" aria-hidden="true"></i>
-                    </span>
-                    <div id="vouchers" class="collapsed">
-                        <form id="unbundle" name="unbundle" action="" method="POST">
-                            {!! method_field('delete') !!}
-                            {!! csrf_field() !!}
-                            <table>
+            </div>
+            <p class="center vh-spaced">You have added {{ $vouchers_amount }} vouchers</p>
+            <button id="collection-button" class="long-button">Go to voucher collection</button>
+            <div class="center">
+                <span class="clickable-span">
+                    Vouchers added
+                    <i class="fa fa-caret-down" aria-hidden="true"></i>
+                </span>
+                <div id="vouchers" class="collapsed">
+                    <form id="unbundle" name="unbundle" action="" method="POST">
+                        {!! method_field('delete') !!}
+                        {!! csrf_field() !!}
+                        <table>
+                            <tr>
+                                <th>Voucher code</th>
+                                <th>Remove</th>
+                            </tr>
+                            @foreach( $vouchers as $voucher )
                                 <tr>
-                                    <th>Voucher code</th>
-                                    <th>Remove</th>
+                                    <td>{{ $voucher->code }}</td>
+                                    <td>
+                                        <button type="submit" formaction="{{ URL::route('store.registration.voucher.delete', ['registration' => $registration->id, 'voucher' => $voucher->id]) }}" id="{{ $voucher->id }}">
+                                            <i class="fa fa-minus" aria-hidden="true"></i>
+                                        </button>
+                                    </td>
                                 </tr>
-                                @foreach( $vouchers as $voucher )
-                                    <tr>
-                                        <td>{{ $voucher->code }}</td>
-                                        <td>
-                                            <button type="submit" formaction="{{ URL::route('store.registration.voucher.delete', ['registration' => $registration->id, 'voucher' => $voucher->id]) }}" id="{{ $voucher->id }}">
-                                                <i class="fa fa-minus" aria-hidden="true"></i>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </table>
-                        </form>
+                            @endforeach
+                        </table>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div id="collection" class="col collection-section">
+            <div>
+                <img src="{{ asset('store/assets/collection-light.svg') }}">
+                <h2>Voucher Pick Up</h2>
+            </div>
+            <div>
+                <p>There are {{ $vouchers_amount }} vouchers waiting for the family</p>
+            </div>
+            <div class="pick-up vh-spaced">
+                <div>
+                    <i class="fa fa-user"></i>
+                    <div>
+                        <label for="collected-by">Collected by:</label>
+                        <select id="collected-by">
+                            @foreach( $carers as $carer )
+                                <option value="{{ $carer->id }}">{{ $carer->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div>
+                    <i class="fa fa-calendar"></i>
+                    <div>
+                        <label for="collected-on">Collected on:</label>
+                        <input id="collected-on" value="<?php echo date('Y-m-d');?>" type="date">
+                    </div>
+                </div>
+                <div>
+                    <i class="fa fa-home"></i>
+                    <div>
+                        <label for="collected-at">Collected at:</label>
+                        <select id="collected-at">
+                            {-- Will need to be a list of all local centres eventually --}
+                            <option value="{{ $centre->id }}">{{ $centre->name }}</option>
+                        </select>
                     </div>
                 </div>
             </div>
-            <div id="collection" class="col collection-section">
-                <div>
-                    <img src="{{ asset('store/assets/collection-light.svg') }}">
-                    <h2>Voucher Pick Up</h2>
-                </div>
-                <div>
-                    <p>There are {{ $vouchers_amount }} vouchers waiting for the family</p>
-                </div>
-                <div class="pick-up vh-spaced">
-                    <div>
-                        <i class="fa fa-user"></i>
-                        <div>
-                            <label for="collected-by">Collected by:</label>
-                            <select id="collected-by">
-                                @foreach( $carers as $carer )
-                                    <option value="{{ $carer->id }}">{{ $carer->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <div>
-                        <i class="fa fa-calendar"></i>
-                        <div>
-                            <label for="collected-on">Collected on:</label>
-                            <input id="collected-on" value="<?php echo date('Y-m-d');?>" type="date">
-                        </div>
-                    </div>
-                    <div>
-                        <i class="fa fa-home"></i>
-                        <div>
-                            <label for="collected-at">Collected at:</label>
-                            <select id="collected-at">
-                                {-- Will need to be a list of all local centres eventually --}
-                                <option value="{{ $centre->id }}">{{ $centre->name }}</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-                <button class="short-button" onclick="javascript:window.location.href='{{ URL::route("store.registration.index") }}'; return false;">Confirm pick up</button>
-                <button class="long-button">Allocate more vouchers</button>
-            </div>
-        <!-- </form> -->
+            <button class="short-button" onclick="javascript:window.location.href='{{ URL::route("store.registration.index") }}'; return false;">Confirm pick up</button>
+            <button class="long-button">Allocate more vouchers</button>
+        </div>
     </div>
 
     @section('hoist-head')
