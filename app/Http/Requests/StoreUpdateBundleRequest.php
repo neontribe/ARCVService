16e916be1e3a9178d1169dcc29187d5ecd84c18a
Charles Strange <charles@neontribe.co.uk>
2018-10-31 16:22:05 +0000

    The front end seems to insist on "Y-m-d" dates in submission, bit "d-m-Y" in input. Adjusted to take repair an assumption it was consistent!

--- app/Http/Requests/StoreUpdateBundleRequest.php
+++ app/Http/Requests/StoreUpdateBundleRequest.php
@@ -33,7 +33,7 @@ class StoreUpdateBundleRequest extends FormRequest
             // MAY be one, can be null, members must be distinct.
             'vouchers.*' => 'nullable|distinct|string',
             // Mutually dependent
-            'collected_on' => 'required_with_all:collected_at,collected_by|date_format:d-m-Y',
+            'collected_on' => 'required_with_all:collected_at,collected_by|date_format:Y-m-d',
             'collected_at' => 'required_with_all:collected_on,collected_by|exists:centres',
             'collected_by' => 'required_with_all:collected_at,collected_on|exists:carers'
         ];
--- tests/Unit/Controllers/Store/BundleControllerTest.php
+++ tests/Unit/Controllers/Store/BundleControllerTest.php
@@ -139,13 +139,13 @@ class BundleControllerTest extends StoreTestCase
         $dataSets = [
             [
                 "data" => [
-                    "collected_at" => "1", "collected_on" => "21-07-2001"
+                    "collected_at" => "1", "collected_on" => "2018-07-21"
                 ],
                 "outcome" => [ "collected_by" => "The collected by field is required when collected at / collected on is present."],
             ],
             [
                 "data" => [
-                    "collected_by" => "1", "collected_on" => "21-07-2001"
+                    "collected_by" => "1", "collected_on" => "2018-07-21"
                 ],
                 "outcome" => [ "collected_at" => "The collected at field is required when collected on / collected by is present."],
             ],
@@ -159,17 +159,17 @@ class BundleControllerTest extends StoreTestCase
                 "data" => [
                     "collected_at" => "1", "collected_on" => "invalid", "collected_by" => "1"
                 ],
-                "outcome" => [ "collected_on" => "The collected on does not match the format d-m-Y."],
+                "outcome" => [ "collected_on" => "The collected on does not match the format Y-m-d."],
             ],
             [
                 "data" => [
-                    "collected_at" => "9999", "collected_on" => "21-07-2001", "collected_by" => "1"
+                    "collected_at" => "9999", "collected_on" => "2018-07-21", "collected_by" => "1"
                 ],
                 "outcome" => [ "collected_at" => "The selected collected at is invalid."],
             ],
             [
                 "data" => [
-                    "collected_at" => "1", "collected_on" => "21-07-2001", "collected_by" => "9999"
+                    "collected_at" => "1", "collected_on" => "2018-07-21", "collected_by" => "9999"
                 ],
                 "outcome" => [ "collected_by" => "The selected collected by is invalid."],
             ],
