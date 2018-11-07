a3ee4fd21e71c807b392546605f44a2d66f49c3d
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-11-07 13:56:34 +0000

    bundle date formatted

--- app/Http/Controllers/Store/HistoryController.php
+++ app/Http/Controllers/Store/HistoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Store;
 
 use App\Http\Controllers\Controller;
+use DateTime;
 use App\Bundle;
 use App\Registration;
 
@@ -11,10 +12,15 @@ class HistoryController extends Controller
     public function show(Registration $registration)
     {
         $pri_carer = $registration->family->carers->all();
+        // $bundle = $registration->bundles;
+        //
+        // $date_collected = DateTime::createFromFormat('Y-m-d', $bundle->disbursed_at);
 
         return view('store.collection_history', [
             'registration' => $registration,
             'pri_carer' => array_shift($pri_carer)
+            // 'bundle' => $bundle,
+            // 'collected_on' => $date_collected->format('d-m-Y')
         ]);
     }
 }
