6ed8dfd788a080bbd6b436a931ca63277b6cd8df
Charles Strange <charles@neontribe.co.uk>
2018-12-12 17:05:08 +0000

    Check the token is good against existing databese ones. Make sure we always get a token, even if uuid isn;t specified on create.

--- app/StateToken.php
+++ app/StateToken.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Ramsey\Uuid\Uuid;
 
 class StateToken extends Model
 {
@@ -23,6 +23,33 @@ class StateToken extends Model
      */
     protected $hidden = [
     ];
+
+    /**
+     * StateToken constructor. Always end up with a UUID
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        // Intercept and make a Uuid
+        $attributes["uuid"] = empty($attributes["uuid"])
+            ? self::generateUnusedToken()
+            : $attributes["uuid"]
+        ;
+
+        // Send that up the chain.
+        parent::__construct($attributes = []);
+    }
+
+    public static function generateUnusedToken()
+    {
+        do {
+            // TODO: Deal with possibility uuid4() may throw an exception of it's own?
+            // TODO: Also, potential infinite loop; i mean, it *shouldn't* happen, right...?
+            // We could throw an exception if iterations > 10, and handle elsewhere
+            $candidate = Uuid::uuid4()->toString();
+        } while (self::where('uuid', '=', $candidate)->exists());
+        return $candidate;
+    }
 
     /**
      * The vouchers that share this StateToken
