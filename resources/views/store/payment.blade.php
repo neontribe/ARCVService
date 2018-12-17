108e4dc513fb5899d634247b1f43d04c6c3b0111
Nikomus <nick.wade@live.co.uk>
2018-12-17 17:27:05 +0000

    feat: stub-out view and tests

---
+++ resources/views/store/payment.blade.php
@@ -0,0 +1,13 @@
+@extends('store.layouts.service_master')
+
+@section('title', 'Voucher Payment')
+
+@section('content')
+
+    @include('store.partials.navbar', ['headerTitle' => 'Voucher Payment'])
+
+    <div class="content payment">
+
+    </div>
+
+@endsection
