ce2b823138d791a824b86c5300009582c995ba44
Nikomus <nick.wade@live.co.uk>
2019-01-07 16:15:18 +0000

    test setup

--- tests/Feature/Store/PaymentPageTest.php
+++ tests/Feature/Store/PaymentPageTest.php
@@ -1,15 +1,49 @@
 <?php
 
+namespace Tests;
+
 use Tests\StoreTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Voucher;
+use App\VoucherState;
+use App\StateToken;
+use App\User;
+use App\Trader;
+use Auth;
 
 class PaymentPageTest extends StoreTestCase
 {
     use DatabaseMigrations;
 
     public function setUp()
+    protected function setUp()
     {
+        parent::setUp();
+        // Login to advance voucher
+        $this->user = factory(User::class)->create();
+        Auth::login($this->user);
 
+        // Create a voucher and a trader with some info
+        $voucher = factory(Voucher::class, 'requested')->create();
+        $trader = factory(Trader::class, 'withnullable')->create();
+        $voucher->code = 'TEST12345';
+        $voucher->trader_id = $trader->id;
+        $voucher->save();
+
+        // Transition to PaymentPending
+        $voucher->applyTransition('order');
+        $voucher->applyTransition('print');
+        $voucher->applyTransition('dispatch');
+        $voucher->applyTransition('collect');
+        $voucher->applyTransition('confirm');
+
+        $stateToken = new StateToken();
+        $stateToken->uuid = "a-real-uuid";
+        $stateToken->save();
+
+        $vs = $voucher->history->last();
+        $vs->stateToken()->associate($stateToken);
+        $vs->save();
     }
 
     /** @test */
