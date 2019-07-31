70b52f71b95423b866e1cce151a47cb2fe61f75e
alexalala <alexmoore94@hotmail.co.uk>
2019-07-31 15:07:30 +0100

    correct print preferences set

--- database/seeds/CentresSeeder.php
+++ database/seeds/CentresSeeder.php
@@ -18,9 +18,9 @@ class CentresSeeder extends Seeder
         $centres = factory(App\Centre::class, 3)->create();
 
         // Grab one and change print pref to individual and another to collection.
-        $centres[0]->print_pref = config('arc.print_preferences.1');
+        $centres[0]->print_pref = config('arc.print_preferences.0');
         $centres[0]->save();
-        $centres[1]->print_pref = config('arc.print_preferences.0');
+        $centres[1]->print_pref = config('arc.print_preferences.1');
         $centres[1]->save();
 
         // 3 centres attached to sponsors with IDs to mirror live data
