326d6c0a9d4073ad1f59c7e11440b5376c7b8611
charlesstrange2 <charles@neontribe.co.uk>
2019-08-03 21:01:21 +0100

    implement store function

--- app/Http/Controllers/Service/Admin/SponsorsController.php
+++ app/Http/Controllers/Service/Admin/SponsorsController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Service\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AdminNewSponsorRequest;
 use App\Sponsor;
+use Auth;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\View\View;
+use Log;
 
 class SponsorsController extends Controller
 {
-
     /**
      * Display a listing of Sponsors.
      *
@@ -33,8 +34,31 @@ class SponsorsController extends Controller
         return view('service.sponsors.create');
     }
 
+    /**
+     * Store a Sponsor
+     *
+     * @param AdminNewSponsorRequest $request
+     * @return \Illuminate\Http\RedirectResponse#
+     */
     public function store(AdminNewSponsorRequest $request)
     {
-        return view('service.sponsors.index');
+        // Validation done already
+        $sponsor = new Sponsor([
+            'name' => $request->input('name'),
+            'shortcode' => $request->input('voucher_prefix'),
+        ]);
+        // Atomic action,don't need to transact it
+        if (!$sponsor->save()) {
+            // Oops! Log that
+            Log::error('Bad save for ' . __CLASS__ . '@' . __METHOD__ . ' by service user ' . Auth::id());
+            // Throw it back to the user
+            return redirect()
+                ->route('admin.sponsors.create')
+                ->withErrors('Creation failed - DB Error.');
+        }
+        // Send to index with a success message
+        return redirect()
+            ->route('admin.sponsors.index')
+            ->with('message', 'Sponsor ' . $sponsor->name . ' created.');
     }
 }
