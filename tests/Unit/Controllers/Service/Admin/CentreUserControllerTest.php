1f7db653a172bb683a8504c70a6fe836063402fb
Charles Strange <charles@neontribe.co.uk>
2019-08-07 10:31:25 +0100

    Add test.

--- tests/Unit/Controllers/Service/Admin/CentreUserControllerTest.php
+++ tests/Unit/Controllers/Service/Admin/CentreUserControllerTest.php
@@ -6,52 +6,99 @@ use App\AdminUser;
 use App\Centre;
 use App\CentreUser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Collection;
 use Tests\StoreTestCase;
 
 class CentreUserControllerTest extends StoreTestCase
 {
     use DatabaseMigrations;
 
+    /** @var AdminUser $adminUser */
+    private $adminUser;
+
+    /** @var Centre $centre */
     private $centre;
 
+    /** @var Collection $altCentres */
     private $altCentres;
+
+    /** @var array $data */
+    private $data;
 
     public function setUp()
     {
         parent::setUp();
+        $this->adminUser = factory(AdminUser::class)->create();
         $this->centre = factory(Centre::class)->create([]);
         $this->altCentres = factory(Centre::class, 3)->create([]);
-    }
-
-    /** @test */
-    public function testICanStoreACentreUser()
-    {
-        $adminUser = factory(AdminUser::class)->create();
-
-        $data = [
+        $this->data = [
             'name' => 'bobby testee',
             'email' => 'bobby@test.co.uk',
             'worker_centre' => $this->centre->id,
             'alternative_centres.*' => $this->altCentres->pluck('id')->all()
         ];
+    }
 
-        $this->actingAs($adminUser, 'admin')
+    /** @test */
+    public function testICanStoreACentreUser()
+    {
+        $this->actingAs($this->adminUser, 'admin')
             ->post(
                 route('admin.centreusers.store'),
-                $data
+                $this->data
             )
             ->followRedirects()
             ->assertResponseOk()
             ->seePageIs(route('admin.centreusers.index'))
-            ->see($data["name"])
-            ->see($data["email"])
+            ->see($this->data["name"])
+            ->see($this->data["email"])
         ;
         // find the user
-        $cu = CentreUser::where('email', $data['email'])->first();
+        $cu = CentreUser::where('email', $this->data['email'])->first();
         $this->assertNotNull($cu);
         // Check the neighbours
         $this->assertEquals(4, $cu->relevantCentres()->count());
         // Check the homeCentre
-        $this->assertEquals($data['worker_centre'], $cu->homeCentre->id);
+        $this->assertEquals($this->data['worker_centre'], $cu->homeCentre->id);
+    }
+
+    /** @test */
+    public function testItCanUpdateACentreUser()
+    {
+        // Make a CentreUser from the data with 1 homeCentre.
+        $cu = factory(CentreUser::class)->create([
+            'name' => "testman",
+            'email' => "testman@test.co.uk",
+        ]);
+        $cu->centres()->attach($this->altCentres->last()->id, ['homeCentre' => true]);
+
+        $this->seeInDatabase('centre_users', [
+            'name' => "testman",
+            'email' => "testman@test.co.uk",
+        ]);
+        // Check that worked.
+        $this->assertCount(1, $cu->centres);
+        $this->assertEquals($this->altCentres->last()->id, $cu->homeCentre->id);
+
+        // Totally update the name, email and centre config from the $data!
+        $this->actingAs($this->adminUser, 'admin')
+            ->post(
+                route('admin.centreusers.store'),
+                $this->data
+            )
+            ->followRedirects()
+            ->assertResponseOk()
+            ->seePageIs(route('admin.centreusers.index'))
+            ->see($this->data["name"])
+            ->see($this->data["email"])
+        ;
+        ;
+        // find the user
+        $cu = CentreUser::where('email', $this->data['email'])->first();
+        $this->assertNotNull($cu);
+        // Check the neighbours
+        $this->assertEquals(4, $cu->relevantCentres()->count());
+        // Check the homeCentre
+        $this->assertEquals($this->data['worker_centre'], $cu->homeCentre->id);
     }
 }
