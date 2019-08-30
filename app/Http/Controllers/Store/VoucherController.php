9e09dbed5d445d344af57c7064ce38b85e23fa10
Harry Dalton <me@hoolean.com>
2019-08-30 18:43:33 +0100

    Use streaming encryption for Master Voucher Log

--- app/Http/Controllers/Store/VoucherController.php
+++ app/Http/Controllers/Store/VoucherController.php
@@ -3,66 +3,28 @@
 namespace App\Http\Controllers\Store;
 
 use App\Http\Controllers\Controller;
+use App\Wrappers\SecretStreamWrapper;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
-use Log;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use URL;
-use ZipArchive;
-use ZipStream\ZipStream;
 
 class VoucherController extends Controller
 {
     // This belongs here because it's largely about arranging vouchers
     public function exportMasterVoucherLog()
     {
-        $dashboard_route = URL::route('store.dashboard');
-
-        // Setup the Storage dir
+        // Setup the Storage dir.
         $disk = Storage::disk(config('arc.mvl_disk'));
         $archiveName = config('arc.mvl_filename');
 
-        // Check it's there.
+        // Check the log exists, so we can error-out before we declare a streamed response.
         if (!$disk->exists($archiveName)) {
             // Return to dashboard with an error that indicates we don't have a file.
-            return redirect($dashboard_route)
+            // TODO : This error copy needs some UI.
+            return redirect(URL::route('store.dashboard'))
                 ->with('error_message', "Sorry, couldn't find a current export. Please check the exporter ran on schedule");
-        }
-
-        // Open and test archive.
-        $storedZip = new ZipArchive();
-
-        // ZipArchive likes a fully qualified path.
-        $check = $storedZip->open($disk->path($archiveName), ZipArchive::CHECKCONS);
-
-        if ($check !== true) {
-            switch ($check) {
-                case ZipArchive::ER_NOZIP:
-                    $message = ('not a zip archive');
-                    break;
-                case ZipArchive::ER_INCONS:
-                    $message = ('consistency check failed');
-                    break;
-                case ZipArchive::ER_CRC:
-                    $message = ('checksum failed');
-                    break;
-                default:
-                    $message = ('error ' . $check);
-            }
-
-            // Log that error
-            Log::error(
-                // Leading `.` as url() gives a leading `/` and the storage is not off root
-                "ZipArchive cannot open archive at '." .
-                // Using url() to prevent revealing the disk tree
-                $disk->url($archiveName) .
-                "' as : " .
-                $message
-            );
-
-            // go back to dashboard with a
-            return redirect($dashboard_route)
-                ->with('error_message', "Sorry, the export file was unreadable. Please contact support.");
         }
 
         // Inject the original file last_modified into the d/l file name.
@@ -71,51 +33,51 @@ class VoucherController extends Controller
             "." . pathinfo($archiveName, PATHINFO_EXTENSION)
         ;
 
-        return new StreamedResponse(function () use ($storedZip) {
-            // Serve the user a zip with all of the files.
-            // We stream this to avoid duplicating everything in memory.
-            $zip = new ZipStream(null, [
-                ZipStream::OPTION_SEND_HTTP_HEADERS => false,
-                ZipStream::OPTION_OUTPUT_STREAM => fopen('php://output', 'w+'),
-            ]);
+        // TODO : refactor SecretStreamWrapper to handle reads, decoupling this controller from crypto code
 
-            // Iterate through files in the zip.
-            // TODO : Change to $zip->count() if we move to PHP 7.2
-            for ($i = 0; $i < $storedZip->numFiles; $i++) {
-                // Get the file's actual name.
-                $sourceName = $storedZip->getNameIndex($i);
+        // Do as much IO as we can comfortably before we begin streaming.
+        $file = fopen($disk->path($archiveName), 'r');
+        $header = fread($file, SODIUM_CRYPTO_SECRETSTREAM_XCHACHA20POLY1305_HEADERBYTES);
 
-                // Open the stream for that file to decrypt it individually.
-                $fileStream = $storedZip->getStream($sourceName);
-                if (!$fileStream) {
-                    Log::error("ZipArchive cannot read compressed item '" . $sourceName . "'' by stream");
-                    continue;
+        // Initialise the decryption stream with its initial header. For more documentation, SecretStreamWrapper.
+        $stream = sodium_crypto_secretstream_xchacha20poly1305_init_pull($header, SecretStreamWrapper::getKey());
+
+        // We want to only keep a small buffer in memory at any time, so our response will be streamed.
+        return new StreamedResponse(function () use ($file, &$stream) {
+            do {
+                // Read the next message.
+                $part = fread($file, SecretStreamWrapper::MESSAGE_SIZE + SODIUM_CRYPTO_SECRETSTREAM_XCHACHA20POLY1305_ABYTES);
+
+                if ($part === false) {
+                    // We couldn't read from the file. Log that as an error.
+                    Log::error("IO error when reading log");
+                    throw new \RuntimeException("IO error when reading log");
                 }
 
-                // Read the file contents to memory; bit of a farce but we don't want to extract to a temp file.
-                // TODO : Can we stream encryption / decryption to reduce memory usage to a buffer?
-                $contents = '';
-                while (!feof($fileStream)) {
-                    $contents .= fread($fileStream, 8192);
+                // Decrypt the message.
+                $part = sodium_crypto_secretstream_xchacha20poly1305_pull($stream, $part);
+
+                if ($part === false) {
+                    // Decryption failed. Log that as an error.
+                    Log::error("Decryption error when reading log");
+                    throw new \RuntimeException("Decryption error when reading log");
                 }
 
-                // Shut that off.
-                fclose($fileStream);
+                // Split the decrypted message into content and metadata.
+                list($message, $tag) = $part;
 
-                // Decrypt the file, if necessary.
-                if (pathinfo($sourceName, PATHINFO_EXTENSION) == 'enc') {
-                    $contents = decrypt($contents);
-                    $destName = pathinfo($sourceName, PATHINFO_FILENAME);
-                } else {
-                    $destName = $sourceName;
+                // The last message should be tagged as such, to ensure there was no tampering after encryption.
+                $eof = feof($file);
+                $lastMessage = $tag === SODIUM_CRYPTO_SECRETSTREAM_XCHACHA20POLY1305_TAG_FINAL;
+                if ($eof != $lastMessage) {
+                    // We met the end of the file before the last message or vice-versa. Log that error.
+                    Log::error("Log read ended prematurely");
+                    throw new \RuntimeException("Log read ended prematurely");
                 }
 
-                // No compression, to start with.
-                $zip->addFile($destName, $contents, [], "store");
-            }
-
-            // Finish the Zip.
-            $zip->finish();
+                // Stream the decrypted content.
+                echo $message;
+            } while (!$eof); // While there is more to do, continue.
         }, 200, [
             'Content-Type' => 'application/x-zip',
             'Content-Disposition' => 'attachment; filename="'. $filename .'"',
@@ -123,6 +85,7 @@ class VoucherController extends Controller
             'Last-Modified' => Carbon::now()->format('D, d M Y H:i:s'),
             'Cache-Control' => 'private, no-cache',
             'Pragma' => 'no-cache',
+            // TODO : Estimate zip size for a progress bar on what is quite a large file
         ]);
     }
 }
