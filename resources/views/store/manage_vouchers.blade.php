d6a26969d938ea75e2e127d94e8520888d93c5df
amandaneontribe <amanda@neontribe.co.uk>
2019-09-05 15:56:06 +0100

    First part of vouchersn autopopulation and attempt of doing second part

--- resources/views/store/manage_vouchers.blade.php
+++ resources/views/store/manage_vouchers.blade.php
@@ -94,10 +94,10 @@
                     {!! csrf_field() !!}
                     <div class="alongside-container">
                         <label>First voucher
-                            <input id="first-voucher" name="start" type="text" autofocus class="uppercase">
+                        <input id="first-voucher" name="start" type="text" data-prefix="{{  $registration->centre->sponsor->shortcode }}" autofocus class="uppercase">
                         </label>
                         <label>Last voucher
-                            <input id="last-voucher" name="end" type="text" class="uppercase">
+                            <input id="last-voucher" name="end" type="text" data-prefix="{{ $registration->centre->sponsor->shortcode }}" class="uppercase">
                         </label>
                         <button id="range-add" class="add-button" type="submit" name="range-add">
                             <i class="fa fa-plus" aria-hidden="true"></i>
@@ -109,7 +109,7 @@
                 {!! csrf_field() !!}
                     <div class="single-container">
                         <label for="single-voucher">Add individual vouchers
-                            <input id="single-voucher" name="start" type="text" class="uppercase">
+                        <input id="single-voucher" name="start" type="text" data-prefix="{{ $registration->centre->sponsor->shortcode }}" class="uppercase">
                         </label>
                         <button id="single-add" class="add-button" type="submit" name="add-button">
                             <i class="fa fa-plus" aria-hidden="true"></i>
@@ -268,7 +268,13 @@
                                 $('#range-add').trigger('click');
                             }
                         }, delay);
-                    }
+                    }                
+                //Vouchers data-prefix autopopulation
+                $("data-prefix").keydown(function (e) {
+                    $("input") .addClass()
+                }
+
+
                 });
 
                 // Handle a single voucher
