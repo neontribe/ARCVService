71e804d2ad1283b45411fb53dcfd8faf2cbffc57
Harry Dalton <me@hoolean.com>
2019-09-09 11:35:23 +0100

    Layout form horizontally, to be consistent

--- resources/views/service/vouchers/create.blade.php
+++ resources/views/service/vouchers/create.blade.php
@@ -9,43 +9,35 @@
 
         <p>Use the form below to add a new batch of vouchers. Select an area, and then enter the starting and ending voucher code numbers.</p>
 
-        <form role="form" method="POST" action="{{ route('admin.vouchers.storebatch') }}" class="styled-form add-vouchers">
+        <form role="form" method="POST" action="{{ route('admin.vouchers.storebatch') }}" class="styled-form">
             {!! csrf_field() !!}
 
-            <div class="select">
-                <label for="sponsor_id">Area</label>
-                <select name="sponsor_id" id="sponsor_id" class="{{ $errors->has('sponsor_id') ? 'has-error' : '' }}" required>
-                    <option value="">Please select an area</option>
-                    @foreach ($sponsors as $sponsor)
-                    <option value="{{ $sponsor->id }}">{{ $sponsor->name }}</option>
-                    @endforeach
-                </select>
+            <div class="horizontal-container">
+                <div class="select">
+                    <label for="sponsor_id">Area</label>
+                    <select name="sponsor_id" id="sponsor_id" class="{{ $errors->has('sponsor_id') ? 'has-error' : '' }}" required>
+                        <option value="">Please select an area</option>
+                        @foreach ($sponsors as $sponsor)
+                        <option value="{{ $sponsor->id }}">{{ $sponsor->name }}</option>
+                        @endforeach
+                    </select>
+                    @if ($errors->has('sponsor_id')) <label for="sponsor_id" class="alert-danger">{{ $errors->first('sponsor_id') }}</label> @endif
+                </div>
+
+                <div>
+                    <label for="start" class="required">Starting voucher code</label>
+                    <input type="text" id="start" name="start" class="{{ $errors->has('start') ? 'error' : '' }}" required>
+                    @if ($errors->has('start')) <label for="start" class="alert-danger">{{ $errors->first('start') }}</label> @endif
+                </div>
+
+                <div>
+                    <label for="end">Ending voucher code</label>
+                    <input type="text" id="end" name="end" class="{{ $errors->has('end') ? 'error' : '' }}" required>
+                    @if ($errors->has('end')) <label for="end" class="alert-danger">{{ $errors->first('end') }}</label> @endif
+                </div>
             </div>
-
-            @if ($errors->has('sponsor_id'))
-            <p class="error">{{ $errors->first('sponsor_id') }}</p>
-            @endif
-
-            <div>
-                <label for="start" class="required">Starting voucher code</label>
-                <input type="text" id="start" name="start" class="{{ $errors->has('start') ? 'error' : '' }}" required>
-            </div>
-
-            @if ($errors->has('start'))
-            <p class="error">{!! $errors->first('start') !!}</p>
-            @endif
-
-            <div>
-                <label for="end">Ending voucher code</label>
-                <input type="text" id="end" name="end" class="{{ $errors->has('end') ? 'error' : '' }}" required>
-            </div>
-
-            @if ($errors->has('end'))
-            <p class="error">{{ $errors->first('end') }}</p>
-            @endif
 
             <button type="submit" id="createVouchers">Create vouchers</button>
-
         </form>
 
     </div>
