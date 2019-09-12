360b4006d71a0f18c2724e3b51851b704e22fd87
Charles Strange <charles@neontribe.co.uk>
2019-09-12 17:14:31 +0100

    WIP store function

--- app/Http/Controllers/Service/Admin/DeliveriesController.php
+++ app/Http/Controllers/Service/Admin/DeliveriesController.php
@@ -7,9 +7,12 @@ use App\Delivery;
 use App\Http\Requests\AdminNewDeliveryRequest;
 use App\Sponsor;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Log;
 
 class DeliveriesController extends Controller
 {
@@ -43,10 +46,43 @@ class DeliveriesController extends Controller
 
     public function store(AdminNewDeliveryRequest $request)
     {
-        $centre = Centre::findOrFail($request->input('centre'));
+        try {
+            $delivery = DB::transaction(function () use ($request) {
 
-        return redirect()
-            ->route('admin.deliveries.index')
-            ->with('message', 'Delivery to ' . $centre->name . ' created.');
+
+                // Update a CentreUser;
+                $centre = Centre::findOrFail($request->input('centre'));
+                $dispatched_at = Carbon::createFromFormat('Y-m-d', $request->input('date-sent'));
+
+                $delivery = Delivery::create([
+                    'centre_id' => $centre->id,
+                    'range' => '',
+                    'dispatched_at'=> $dispatched_at
+                ]);
+
+                $range
+
+                return $delivery;
+            });
+        } catch (Exception $e) {
+            // Oops! Log that
+            Log::error('Bad transaction for ' . __CLASS__ . '@' . __METHOD__ . ' by service user ' . Auth::id());
+            Log::error($e->getTraceAsString());
+            // Throw it back to the user
+
+            return redirect()
+                ->route('admin.deliveries.index')
+                ->with('message', 'Delivery to ' . $centre->name . ' created.');
+        }
+        // Create delivery
+
+        // Create ranges of deliverable vouchers
+            // they are printed
+            // they are not on a delivery
+
+        // If ranges have problems, fail and say why
+
+        // Progress state of each voucher in the range to delvierable
+
     }
 }
