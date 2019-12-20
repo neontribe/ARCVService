284fe10dd00f985c763a7a620b6d6b09ed5bfeab
Charles Strange <charles@neontribe.co.uk>
2019-12-20 17:38:32 +0000

    WIP - route is not setting a resolver., and is returning true

--- app/Policies/Store/RegistrationPolicy.php
+++ app/Policies/Store/RegistrationPolicy.php
@@ -5,14 +5,16 @@ namespace App\Policies\Store;
 use App\CentreUser;
 use App\Registration;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Log;
 
 class RegistrationPolicy
 {
     use HandlesAuthorization;
 
     // Permission to view a specific registration, based on centre.
-    public function view(CentreUser $user, Registration $registration)
+    public function viewAndEdit(CentreUser $user, Registration $registration)
     {
-        return $user->isRelevantCentre($registration->centre);
+        return true;
+        //return $user->isRelevantCentre($registration->homeCentre);
     }
 }
