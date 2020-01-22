8127ce7ab9b1be0895c733d96c09ee80ebf71eb3
Charles Strange <charles@neontribe.co.uk>
2020-01-22 10:34:06 +0000

    1.8.2 - HOTFIX
    Add a download policy, and refactor the routes and dashboard to use it.

--- app/Policies/Store/CentrePolicy.php
+++ app/Policies/Store/CentrePolicy.php
@@ -10,11 +10,10 @@ class CentrePolicy
 {
     use HandlesAuthorization;
 
-    // permission to export only relevant things.
-    public function exportFromRelevantCentre(CentreUser $user, Centre $centre)
+    // Can view the relevant centre...
+    public function viewRelevantCentre(CentreUser $user, Centre $centre)
     {
-        $canDownload = ($user->downloader) ?? false;
-        $canAccess = $user->isRelevantCentre($centre);
-        return ($canAccess && $canDownload);
+        // ...because it's ours, or a neighbor
+        return $user->isRelevantCentre($centre);
     }
 }
