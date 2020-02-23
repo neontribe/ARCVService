056b3136b5d11ba76406daab9832e1e228fa19f5
charlesstrange2 <25037036+charlesstrange2@users.noreply.github.com>
2020-02-23 22:27:58 +0000

    Update comments for PR
    This is what happens when you copy paste a similar class and dont read your own comments...

--- app/Services/VoucherEvaluator/Evaluations/FamilyHasUnverifiedChildren.php
+++ app/Services/VoucherEvaluator/Evaluations/FamilyHasUnverifiedChildren.php
@@ -9,21 +9,22 @@ class FamilyHasUnverifiedChildren extends BaseFamilyEvaluation
 {
     public $reason = 'has children needing ID';
     private $specification;
+
     /**
-     * FamilyIsPregnant constructor.
+     * FamilyHasUnverifiedChildren constructor.
      * @param Carbon|null $offsetDate
      */
     public function __construct(Carbon $offsetDate = null)
     {
         parent::__construct($offsetDate);
 
-        // Pregnancies or under school age.
+        // Child is Verified specification
         $this->specification = new IsVerified();
     }
 
     /**
      * @param $candidate
-     * @return FamilyHasNoEligibleChildren|void|null
+     * @return FamilyHasUnverifiedChildren|void|null
      */
     public function test($candidate)
     {
