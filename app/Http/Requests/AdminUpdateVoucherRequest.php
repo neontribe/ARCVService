69c79250885c453dbf3d9c386f78e49f2b479fe7
Charles Strange <charles@neontribe.co.uk>
2020-03-04 14:55:49 +0000

    Fix voiding form and formrequest

--- app/Http/Requests/AdminUpdateVoucherRequest.php
+++ app/Http/Requests/AdminUpdateVoucherRequest.php
@@ -2,25 +2,58 @@
 
 namespace App\Http\Requests;
 
+use App\Voucher;
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class AdminUpdateVoucherRequest extends AdminNewVoucherRequest
+class AdminUpdateVoucherRequest extends FormRequest
 {
     /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // Should be route protected.
+        return true;
+    }
+
+    /**
      * Get the validation rules that apply to the request.
-     * Append ours to the parent ones
      *
      * @return array
      */
     public function rules()
     {
-        $additionalRules = [
+        return  [
+            // MUST be present, string, exists
+            'voucher-start' => 'required|string|exists:vouchers,code',
+            // MUST be present, string, exists, greater/equal to start and same sponsor as start
+            'voucher-end' => 'required|string|exists:vouchers,code|codeGreaterThan:voucher-start|sameSponsor:voucher-start',
             // MUST be present AND be in a subset of transitions
             'transition' => [
                 'required',
-                Rule::in(['expire', 'lose']),
+                Rule::in(['expire', 'void']),
             ],
         ];
-        return  array_merge(parent::rules(), $additionalRules);
+    }
+
+    protected function prepareForValidation()
+    {
+        // get the input and remove null/empty values.
+        $input = array_filter(
+            $this->all(['voucher-start', 'voucher-end', 'transition']),
+            'strlen'
+        );
+
+        foreach ($input as $key => $value) {
+            if (in_array($key, ['voucher-start', 'voucher-end'])) {
+                $clean = Voucher::cleanCodes((array)$value);
+                $input[$key] = strtoupper((array_shift($clean)));
+            }
+        }
+        // replace old input with new input
+        $this->replace($input);
     }
 }
--- resources/views/service/vouchers/void.blade.php
+++ resources/views/service/vouchers/void.blade.php
@@ -1,59 +1,55 @@
 @extends('service.layouts.app')
 @section('content')
 
-<div id="container">
-    @include('service.includes.sidebar')
-    <div id="main-content">
+    <div id="container">
+        @include('service.includes.sidebar')
+        <div id="main-content">
 
-        <h1>Void a batch of voucher codes</h1>
-
-        <p>Use the form below to <strong>void</strong> a batch of vouchers. Select an area, and then enter the starting and ending voucher code numbers.</p>
-
-        <form role="form" method="POST" action="{{ route('admin.vouchers.updatebatch') }}" class="styled-form">
-            {!! method_field("PATCH") !!}
-            {!! csrf_field() !!}
-
-            <div class="horizontal-container">
-                <div class="select">
-                    <label for="sponsor_id">Area</label>
-                    <select name="sponsor_id" id="sponsor_id" class="{{ $errors->has('sponsor_id') ? 'has-error' : '' }}" required>
-                        <option value="">Please select an area</option>
-                        @foreach ($sponsors as $sponsor)
-                            <option value="{{ $sponsor->id }}" @if( old('sponsor_id') == $sponsor->id ) SELECTED @endif >{{ $sponsor->name }}</option>
-                        @endforeach
-                    </select>
-                    @if ($errors->has('sponsor_id'))
-                        <label for="sponsor_id" class="alert-danger">{{ $errors->first('sponsor_id') }}</label>
-                    @endif
+            <h1>Void vouchers</h1>
+            @if (Session::get('error_message'))
+                <div class="alert alert-danger">
+                    {{ Session::get('error_message') }}
                 </div>
+            @endif
+            <p>Use the form below to mark a batch of vouchers as "Void" or "Expired". Add start and end voucher codes of the batch.</p>
 
-                <div>
-                    <label for="start" class="required">Starting voucher code</label>
-                    <input type="text" id="start" name="start" value="{{ old('start') }}" class="{{ $errors->has('start') ? 'error' : '' }}" required>
-                    @if ($errors->has('start')) <label for="start" class="alert-danger">{{ $errors->first('start') }}</label> @endif
-                    @if ($errors->has('start-serial')) <label for="start" class="alert-danger">{{ $errors->first('start-serial') }}</label> @endif
-                </div>
-
-                <div>
-                    <label for="end">Ending voucher code</label>
-                    <input type="text" id="end" name="end" value="{{ old('end') }}" class="{{ $errors->has('end') ? 'error' : '' }}" required>
-                    @if ($errors->has('end')) <label for="end" class="alert-danger">{{ $errors->first('end') }}</label> @endif
-                    @if ($errors->has('end-serial')) <label for="start" class="alert-danger">{{ $errors->first('end-serial') }}</label> @endif
-                </div>
-            </div>
-            <div class="container">
-                <div classs="row">
-                    <div class="col-sm-5">
-                        <button type="submit" name="transition" value="expire">Expire vouchers</button>
+            <form role="form" class="styled-form" method="POST" action="{{ route('admin.vouchers.updatebatch') }}">
+                {!! method_field("PATCH") !!}
+                {!! csrf_field() !!}
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-lg-3">
+                            <label for="voucher-start" class="required">Start Voucher</label>
+                            <input type="text"
+                                   id="voucher-start"
+                                   name="voucher-start"
+                                   value="{{ old('voucher-start') }}"
+                                   class="{{ $errors->has('voucher-start') ? 'error' : '' }} uppercase"
+                                   required >
+                            @include('service.partials.validationMessages', array('inputName' => 'voucher-start'))
+                        </div>
+                        <div class="col-sm-6 col-lg-9">
+                            <label for="voucher-end" class="required">End Voucher</label>
+                            <input type="text"
+                                   id="voucher-end"
+                                   name="voucher-end"
+                                   value="{{ old('voucher-end') }}"
+                                   class="{{ $errors->has('voucher-end') ? 'error' : '' }} uppercase"
+                                   required >
+                            @include('service.partials.validationMessages', array('inputName' => 'voucher-end'))
+                        </div>
                     </div>
-                    <div class="col-sm-7">
-                        <button type="submit" name="transition" value="lose">Void vouchers</button>
+                    <div class="row">
+                        <div class="col-sm-6 col-lg-3">
+                            <button type="submit" name="transition" value="expire">Expire vouchers</button>
+                        </div>
+                        <div class="col-sm-6 col-lg-9">
+                            <button type="submit" name="transition" value="lose">Void vouchers</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </form>
-
+            </form>
+        </div>
     </div>
-</div>
 
 @endsection
