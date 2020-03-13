783de765ceb9f8ecca25d4ccf27cc72f8b4a171a
Charles Strange <charles@neontribe.co.uk>
2020-03-13 17:26:06 +0000

    Clarify with comments.

--- app/Specifications/IsUnderStartDate.php
+++ app/Specifications/IsUnderStartDate.php
@@ -21,9 +21,9 @@ class IsUnderStartDate extends AbstractSpecification
     /**
      * IsUnderStartDate constructor.
      *
-     * @param $offsetDate
-     * @param int $yearsAhead
-     * @param int $offsetMonth
+     * @param Carbon $offsetDate Usually today, unless a test had changed that.
+     * @param int $yearsAhead How many years away we want to look
+     * @param int $offsetMonth What month the event will be, usually configured to 9
      */
     public function __construct($offsetDate, int $yearsAhead, int $offsetMonth)
     {
@@ -33,13 +33,16 @@ class IsUnderStartDate extends AbstractSpecification
     }
 
     /**
-    * Tests an object and returns a boolean value
-    * @param Child $candidate
-    * @return  Boolean
-    */
+     * Tests an object and returns a boolean value
+     * @param Child $candidate
+     * @return  Boolean
+     */
     public function isSatisfiedBy(Child $candidate)
     {
+        // Generate the date of the event in question
         $targetDate = $candidate->calcFutureMonthYear($this->yearsAhead, $this->offsetMonth);
+
+        // return (bool) if the offset date is less than that.
         return $this->offsetDate->lessThan($targetDate);
     }
 }
