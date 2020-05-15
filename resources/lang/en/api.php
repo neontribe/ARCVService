d9d30494fbda0863911ccdb9e9ecd558437fec1d
Charles Strange <charles@neontribe.co.uk>
2020-05-15 16:30:49 +0100

    Add copy markers

--- resources/lang/en/api.php
+++ resources/lang/en/api.php
@@ -14,8 +14,8 @@ return [
     'errors' => [
         'invalid_credentials' => 'The user credentials were incorrect.',
         'voucher_own_dupe' => 'You have already submitted voucher code :code.',
-        'voucher_other_dupe' => 'It looks like the code (:code) has been used already, please double check and try again. If you are still unable to add the voucher code, don\'t worry you will still be paid. Please email admin@alexandrarose.org.uk to let us know that this happened and give the voucher number.',
+        'voucher_other_dupe' => '[cCc]It looks like the code (:code) has been used already, please double check and try again. If you are still unable to add the voucher code, don\'t worry you will still be paid. Please email admin@alexandrarose.org.uk to let us know that this happened and give the voucher number.[cCc]',
         'voucher_failed_reject' => 'I\'m sorry, we couldn\'t undo that for you at this time. Let us know about this code when you send in your vouchers.',
-        'voucher_unavailable' => 'Sorry that code is not available. Please check the code including the letters. You can change the letters when you enter the code by using the backspace key.',
+        'voucher_unavailable' => '[cCc]Sorry that code is not available. Please check the code including the letters. You can change the letters when you enter the code by using the backspace key.[cCc]',
     ]
 ];
