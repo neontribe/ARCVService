0aa2bc73e10dcd55c5d81cd585c952cda396e989
Charles Strange <charles@neontribe.co.uk>
2020-05-19 16:58:16 +0100

    Set migration, model and relationship

---
+++ app/Evaluation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Evaluation extends Model
+{
+    // An evaluation is currently for a sponsor
+}
---
+++ database/migrations/2020_05_19_160944_create_evaluations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvaluationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sponsor_id');
+            $table->string('name');
+            $table->integer('value')->nullable();
+            $table->timestamps();
+            $table->unique(['sponsor_id','name'], 'unique_sponsor_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('evaluations')) {
+            Schema::table('sponsors', function (Blueprint $table) {
+                $table->dropUnique('unique_sponsor_name');
+            });
+            Schema::drop('evaluations');
+        }
+    }
+}
