0caf43811ad999e0079f61559bde304051ed57a9
katjam <katja@neontribe.co.uk>
2020-05-21 17:41:04 +0100

    Only remove deprecated coucher states in production env

--- database/migrations/2020_05_21_094708_remove_ordered_printed_from_voucher_states_table.php
+++ database/migrations/2020_05_21_094708_remove_ordered_printed_from_voucher_states_table.php
@@ -14,12 +14,17 @@ class RemoveOrderedPrintedFromVoucherStatesTable extends Migration
     public function up()
     {
         Schema::table('voucher_states', function (Blueprint $table) {
+            // We only want to remove this data to production environments.
+            // Seeded environments will not contain these deprecated states.
+            if (!App::environment('production')) {
+                return;
+            }
+
             // I guess nice to have a record of how many rows were deleted?
             $number_to_delete = App\VoucherState::whereIn('to', ['ordered', 'printed'])->count();
-            if ($number_to_delete > 0) {
-                App\VoucherState::whereIn('to', ['ordered', 'printed'])->delete();
-                Log::info($number_to_delete . ' printed and ordered voucher_states records deleted on migration.');
-            }
+            App\VoucherState::whereIn('to', ['ordered', 'printed'])->delete();
+            Log::info($number_to_delete . ' printed and ordered voucher_states records deleted on migration.');
+
         });
     }
 
@@ -35,6 +40,7 @@ class RemoveOrderedPrintedFromVoucherStatesTable extends Migration
             // We could alternatively reconstruct them based on the voucher created_at.
             // This would include some moving around of data since at least 185000 vouchers
             // were missing these states - and we need ids to be same order as timestamps.
+            return;
         });
     }
 }
