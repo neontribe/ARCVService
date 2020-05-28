a829a5502bdafceb0e17211b6ea6724bed0024c6
Charles Strange <charles@neontribe.co.uk>
2020-05-28 11:49:44 +0100

    Fixes for PR

--- app/Evaluation.php
+++ app/Evaluation.php
@@ -26,6 +26,9 @@ class Evaluation extends Model
      */
     public function sponsor()
     {
+        // Oddly saveMany was giving issues until this relationship was two way.
+        // Not entirely sure why, but this fixed it.
+        // TODO: review and amend as necessary
         return $this->belongsTo('App\Sponsor');
     }
 }
