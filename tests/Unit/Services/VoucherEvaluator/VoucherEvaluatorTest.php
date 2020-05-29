b2ecb0e26ad9141173fe1c9d0ae149fd75017f80
Alin <alin@neontribe.co.uk>
2020-05-29 15:53:40 +0100

    Refactor test to dry it up, add more assertions

--- tests/Unit/Services/VoucherEvaluator/VoucherEvaluatorTest.php
+++ tests/Unit/Services/VoucherEvaluator/VoucherEvaluatorTest.php
@@ -36,6 +36,17 @@ class VoucherEvaluatorTest extends TestCase
 
     private $rulesMods = [];
 
+    private $family;
+    private $pregnancy;
+    private $isPrimarySchool;
+    private $isOverPrimarySchool;
+    private $underPrimarySchool;
+    private $underOne;
+    private $isSecondarySchoolAge;
+    private $isAlmostOne;
+    private $readyForPrimarySchool;
+    private $readyForSecondarySchool;
+
     protected function setUp()
     {
         parent::setUp();
@@ -96,16 +107,24 @@ class VoucherEvaluatorTest extends TestCase
                 "entity" => "App\Family",
             ]),
         ];
+
+        $this->family = factory(Family::class)->create();
+        $this->pregnancy = factory(Child::class, 'unbornChild')->make();
+        $this->isPrimarySchool = factory(Child::class, 'isPrimarySchoolAge')->make();
+        $this->isOverPrimarySchool = factory(Child::class, 'isSecondarySchoolAge')->make();
+        $this->underPrimarySchool = factory(Child::class, 'betweenOneAndPrimarySchoolAge')->make();
+        $this->underOne = factory(Child::class, 'underOne')->make();
+        $this->isSecondarySchoolAge = factory(Child::class, 'isSecondarySchoolAge')->make();
+        $this->isAlmostOne = factory(Child::class, 'almostOne')->make();
+        $this->readyForPrimarySchool = factory(Child::class, 'readyForPrimarySchool')->make();
+        $this->readyForSecondarySchool = factory(Child::class, 'readyForSecondarySchool')->make();
     }
 
     /** @test */
     public function itNoticesWhenAFamilyStillRequiresIDForChildren()
     {
-        // Create a family with kids that have not been verified
-        $family = factory(Family::class)->create();
-
         $unverifiedKids = factory(Child::class, 3)->states('unverified')->make();
-        $family->children()->saveMany($unverifiedKids);
+        $this->family->children()->saveMany($unverifiedKids);
 
         // Get rules Mods
         $rulesMods = collect($this->rulesMods["notice-unverified-kids"]);
@@ -114,20 +133,20 @@ class VoucherEvaluatorTest extends TestCase
         $evaluator = EvaluatorFactory::make($rulesMods);
 
         // Evaluate the family
-        $evaluation = $evaluator->evaluate($family);
+        $evaluation = $evaluator->evaluate($this->family);
         $notices = $evaluation["notices"];
 
         // There should be a notice reason of 'FamilyHasUnverifiedChildren'
         $this->assertContains(self::NOTICE_TYPES['FamilyHasUnverifiedChildren'], $notices);
 
         // Set them all verified
-        $family->children->each(function ($child) {
+        $this->family->children->each(function ($child) {
             $child->verified = true;
             $child->save();
         });
 
         // Evaluate the family again.
-        $evaluation2 = $evaluator->evaluate($family);
+        $evaluation2 = $evaluator->evaluate($this->family);
         $notices = $evaluation2["notices"];
 
         // There should NOT be a notice reason of 'FamilyHasUnverifiedChildren'
@@ -137,15 +156,11 @@ class VoucherEvaluatorTest extends TestCase
     /** @test */
     public function itCreditsWhenAFamilyIsPregnant()
     {
-        // Make a pregnant family
-        $family = factory(Family::class)->create();
-
-        $pregnancy = factory(Child::class, 'unbornChild')->make();
-        $family->children()->save($pregnancy);
+        $this->family->children()->save($this->pregnancy);
 
         // Make standard evaluator
         $evaluator = EvaluatorFactory::make();
-        $evaluation = $evaluator->evaluate($family);
+        $evaluation = $evaluator->evaluate($this->family);
         $credits = $evaluation["credits"];
 
         // There should be a credit reason of 'FamilyIsPregnant'
@@ -162,18 +177,11 @@ class VoucherEvaluatorTest extends TestCase
         // Make evaluator
         $evaluator = EvaluatorFactory::make($rulesMods);
 
-        // Make our family
-        $family = factory(Family::class)->create();
-
-        // Make kids
-        $isPrimarySchool = factory(Child::class, 'isPrimarySchoolAge')->make();
-        $isOverPrimarySchool = factory(Child::class, 'isSecondarySchoolAge')->make();
-
         // Add the kids and check they saved
-        $family->children()->saveMany([$isPrimarySchool, $isOverPrimarySchool]);
-        $this->assertEquals(2, $family->children()->count());
+        $this->family->children()->saveMany([$this->isPrimarySchool, $this->isOverPrimarySchool]);
+        $this->assertEquals(2, $this->family->children()->count());
 
-        $evaluation = $evaluator->evaluate($family);
+        $evaluation = $evaluator->evaluate($this->family);
 
         // Check it can find eligible children (0 vouchers)
         // - because no under primary school-ers validate the primary school-ers.
@@ -190,30 +198,20 @@ class VoucherEvaluatorTest extends TestCase
         // Make evaluator
         $evaluator = EvaluatorFactory::make($rulesMods);
 
-        // Make our family
-        $family = factory(Family::class)->create();
-
-        // Make a set of ineligible kids (no under primary school age qualifiers)
-        $isPrimarySchool = factory(Child::class, 'isPrimarySchoolAge')->make();
-        $isOverPrimarySchool = factory(Child::class, 'isSecondarySchoolAge')->make();
-
-        // Add the kids and check they saved
-        $family->children()->saveMany([$isPrimarySchool, $isOverPrimarySchool]);
-        $this->assertEquals(2, $family->children()->count());
+        // Add a set of ineligible kids (no under primary school age qualifiers) and check they saved
+        $this->family->children()->saveMany([$this->isPrimarySchool, $this->isOverPrimarySchool]);
+        $this->assertEquals(2, $this->family->children()->count());
 
         // Run the evaluation
-        $evaluation = $evaluator->evaluate($family);
+        $evaluation = $evaluator->evaluate($this->family);
         // Check it can't find any eligible children (0 vouchers)
         // - because no under primary school-ers validate the primary school-ers.
         $this->assertFalse($evaluation->getEligibility());
         $this->assertEquals('0', $evaluation->getEntitlement());
 
-        // Now, add a kid that will make the child at primary school age qualified.
-        $underPrimarySchool = factory(Child::class, 'betweenOneAndPrimarySchoolAge')->make();
-
-        // Re-save
-        $family->children()->saveMany([$underPrimarySchool, $isPrimarySchool, $isOverPrimarySchool]);
-        $family = $family->fresh();
+        // Re-save with a kid that will make the child at primary school age qualified
+        $this->family->children()->saveMany([$this->underPrimarySchool, $this->isPrimarySchool, $this->isOverPrimarySchool]);
+        $family = $this->family->fresh();
 
         // Check we've saved the children correctly
         $this->assertEquals(3, $family->children->count());
@@ -234,12 +232,9 @@ class VoucherEvaluatorTest extends TestCase
     /** @test */
     public function itCreditsWhenAChildIsUnderOne()
     {
-        // Make a Child under one.
-        $child = factory(Child::class, 'underOne')->make();
-
-        // Make standard evaluator
+        // Make standard evaluator for a child under one
         $evaluator = EvaluatorFactory::make();
-        $evaluation = $evaluator->evaluate($child);
+        $evaluation = $evaluator->evaluate($this->underOne);
         $credits = $evaluation["credits"];
 
         $this->assertEquals(1, count($credits));
@@ -252,12 +247,9 @@ class VoucherEvaluatorTest extends TestCase
     /** @test */
     public function itCreditsWhenAChildIsBetweenOneAndPrimarySchoolAge()
     {
-        // Make a Child under School Age.
-        $child = factory(Child::class, 'betweenOneAndPrimarySchoolAge')->make();
-
-        // Make standard evaluator
+        // Make standard evaluator for a child under school age
         $evaluator = EvaluatorFactory::make();
-        $evaluation = $evaluator->evaluate($child);
+        $evaluation = $evaluator->evaluate($this->underPrimarySchool);
         $credits = $evaluation["credits"];
 
         // Check there's one, because child is not under one.
@@ -271,14 +263,11 @@ class VoucherEvaluatorTest extends TestCase
     /** @test */
     public function itDoesNotCreditWhenAChildisSecondarySchoolAge()
     {
-        // Make a Secondary school child
-        $child = factory(Child::class, 'isSecondarySchoolAge')->make();
-
         $rulesMod = collect($this->rulesMods["credit-primary"]);
 
         // Make extended evaluator
         $evaluator = EvaluatorFactory::make($rulesMod);
-        $evaluation = $evaluator->evaluate($child);
+        $evaluation = $evaluator->evaluate($this->isSecondarySchoolAge);
         $credits = $evaluation["credits"];
 
         // Check there's none, because child is not primary or under.
@@ -289,12 +278,9 @@ class VoucherEvaluatorTest extends TestCase
     /** @test */
     public function itNoticesWhenAChildIsAlmostOne()
     {
-        // Make a Child under School Age.
-        $child = factory(Child::class, 'almostOne')->make();
-
         // Make standard evaluator
         $evaluator = EvaluatorFactory::make();
-        $evaluation = $evaluator->evaluate($child);
+        $evaluation = $evaluator->evaluate($this->isAlmostOne);
         $notices = $evaluation["notices"];
 
 
@@ -312,11 +298,9 @@ class VoucherEvaluatorTest extends TestCase
         // Need to change the values we use for school start to next month's integer
         Config::set('arc.school_month', Carbon::now()->addMonth(1)->month);
 
-        $child = factory(Child::class, 'readyForPrimarySchool')->make();
-
         // Make standard evaluator
         $evaluator = EvaluatorFactory::make();
-        $evaluation = $evaluator->evaluate($child);
+        $evaluation = $evaluator->evaluate($this->readyForPrimarySchool);
         $notices = $evaluation["notices"];
 
         // Check there's one, because no other event is pending.
@@ -333,14 +317,12 @@ class VoucherEvaluatorTest extends TestCase
         // Need to change the values we use for school start to next month's integer
         Config::set('arc.school_month', Carbon::now()->addMonth(1)->month);
 
-        $child = factory(Child::class, 'readyForSecondarySchool')->make();
-
         // Get Rules Mods
         $rulesMod = collect($this->rulesMods["credit-primary"]);
 
         // Make standard evaluator
         $evaluator = EvaluatorFactory::make($rulesMod);
-        $evaluation = $evaluator->evaluate($child);
+        $evaluation = $evaluator->evaluate($this->readyForSecondarySchool);
         $notices = $evaluation["notices"];
 
         // Check there's one
@@ -382,29 +364,47 @@ class VoucherEvaluatorTest extends TestCase
     }
 
     /** @test */
-    public function itHasADefaultSetOfRules()
+    public function itHasADefaultSetOfRulesAndCanAcceptVariableValuesForEvaluations()
     {
-        $family = factory(Family::class)->create();
+        // We make a registration in a non-SK area
         $centre = factory(Centre::class)->create();
 
         $registration = new Registration();
         $registration->centre_id = $centre->id;
         $registration->eligibility = "other";
-        $registration->family_id = $family->id;
+        $registration->family_id = $this->family->id;
         $registration->save();
 
-        $pregnancy = factory(Child::class, 'unbornChild')->make();
-        $underOneChild = factory(Child::class, 'underOne')->make();
-        $toddler = factory(Child::class, 'betweenOneAndPrimarySchoolAge')->make();
-        $kidInPrimarySchool = factory(Child::class, 'isPrimarySchoolAge')->make();
-        $children = [$pregnancy, $underOneChild, $toddler, $kidInPrimarySchool];
+        // Add some children to the family
+        $children = [$this->pregnancy, $this->underOne, $this->underPrimarySchool, $this->isPrimarySchool];
 
-        $family->children()->saveMany($children);
-        $this->assertEquals(4, $family->children->count());
+        $this->family->children()->saveMany($children);
+        // Test family has 4 children, including a pregnancy
+        $this->assertEquals(4, $this->family->children->count());
 
         $evaluator = EvaluatorFactory::make();
-        $evaluation = $evaluator->evaluate($family);
+        $evaluation = $evaluator->evaluate($this->family);
 
+        // Test we get a default number of total vouchers
         $this->assertEquals(12, $evaluation->getEntitlement());
+
+        // Change number of vouchers allocated to a pregnant family
+        $this->rulesMods['pregnancy'] = [
+            new Evaluation([
+            'name' => 'FamilyIsPregnant',
+            'value' => 4,
+            'purpose' => 'credits',
+            'entity' => 'App\Family',
+            'sponsor_id' => $registration->centre->sponsor->id,
+            ])
+        ];
+
+        $evaluator = EvaluatorFactory::make(collect($this->rulesMods['pregnancy']));
+        $newFamilyEvaluation = $evaluator->evaluate($this->family);
+        $credits = $newFamilyEvaluation['credits'];
+        // We test we get more vouchers with the new rule
+        $this->assertNotEquals(self::CREDIT_TYPES['FamilyIsPregnant']['value'], $credits[0]['value']);
+        $this->assertEquals(4, $credits[0]['value']);
+        $this->assertEquals(13, $newFamilyEvaluation->getEntitlement());
     }
 }
