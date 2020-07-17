646a64a890ffd2828e850227bc7b30b90a2b4548
Charles Strange <charles@neontribe.co.uk>
2020-07-17 15:29:20 +0100

    Update CORS packages and set new configs

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Fruitcake\Cors\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -21,8 +22,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\XssHeader::class,
         \App\Http\Middleware\ContentTypeOptionsHeader::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Barryvdh\Cors\HandleCors::class,
-
     ];
 
     /**
