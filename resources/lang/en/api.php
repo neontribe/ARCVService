8e8a98734dedd23e767c51b405fe842b2fc36f9d
Alin <alin@neontribe.co.uk>
2021-01-12 17:17:10 +0000

    Remove method

--- resources/lang/en/api.php
+++ resources/lang/en/api.php
@@ -8,7 +8,7 @@ return [
         'batch_voucher_submit' => 'Thanks! Your queue has been successfully submitted. :success_amount voucher(s) accepted, :duplicate_amount duplicate(s) and :invalid_amount invalid.',
         'email_voucher_history' => 'Thanks. If you don\'t receive an email with your voucher records, please try again later.',
         'email_voucher_history_date' => 'Thanks, we\'ve received your request for payment records from :date. If you don\'t receive an email with your voucher records, please try again later.',
-        'voucher_payment_requested' => 'Thanks, your payment request has been accepted. :payment_request_message',
+        'voucher_payment_requested' => 'Thanks, your payment request has been accepted.',
     ],
 
     'errors' => [
