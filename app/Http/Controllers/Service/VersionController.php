80953643e9dae0df04e4682b9d056f056796cb6c
neonleann <93125276+neonleann@users.noreply.github.com>
2022-03-08 10:15:10 +0000

    Switch to composer.json.

--- app/Http/Controllers/Service/VersionController.php
+++ app/Http/Controllers/Service/VersionController.php
@@ -17,7 +17,7 @@ class VersionController extends Controller
     public function version(): JsonResponse
     {
         /** @noinspection PhpComposerExtensionStubsInspection */
-        $pkg = json_decode(file_get_contents(base_path('package.json')), false);
+        $pkg = json_decode(file_get_contents(base_path('composer.json')), false);
 
         return response()->json(['Service/API' => $pkg->version]);
     }
