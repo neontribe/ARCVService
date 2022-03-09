e5a30e2f058d085526d15d2f8ed0528d2f437d73
neonleann <93125276+neonleann@users.noreply.github.com>
2022-03-09 14:33:04 +0000

    Fixing the 'are they at school' rule.

--- app/Services/VoucherEvaluator/Evaluations/ScottishChildIsPrimarySchoolAge.php
+++ app/Services/VoucherEvaluator/Evaluations/ScottishChildIsPrimarySchoolAge.php
@@ -28,17 +28,24 @@ class ScottishChildIsPrimarySchoolAge extends BaseChildEvaluation
     public function test($candidate)
     {
         parent::test($candidate);
-        $format = '%y,%m';
-        $age = $candidate->getAgeString($format);
-        $arr = explode(",", $age, 2);
-        if ($arr[0] == 'P') {
+        $monthNow = Carbon::now()->month;
+        $schoolStartMonth = config('arc.scottish_school_month');
+        // Has the start month gone?
+        if ($schoolStartMonth - $monthNow < 0) {
+          $format = '%y,%m';
+          $age = $candidate->getAgeString($format);
+          $arr = explode(",", $age, 2);
+          if ($arr[0] == 'P') {
+            return $this->fail();
+          }
+          $year = $arr[0];
+          $month = $arr[1];
+          $isAtSchool = false;
+          if ((($year == 4 && $month >=1) || $year >= 5) && !$candidate->deferred) {
+            $isAtSchool = true;
+          }
+        } else {
           return $this->fail();
-        }
-        $year = $arr[0];
-        $month = $arr[1];
-        $isAtSchool = false;
-        if ((($year == 4 && $month >=1) || $year <= 5) && !$candidate->deferred) {
-          $isAtSchool = true;
         }
 
         return ($this->specification->isSatisfiedBy($candidate) && $isAtSchool)
