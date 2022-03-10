d7bbbf5b0fd7331e77c397bc06e4c804ac29f567
Charles Strange <charles@neontribe.co.uk>
2022-03-10 10:31:15 +0000

    seperate updates from column changes.

--- database/migrations/2022_02_23_161002_update_registrations_table.php
+++ database/migrations/2022_02_23_161002_update_registrations_table.php
@@ -13,38 +13,38 @@ class UpdateRegistrationsTable extends Migration
      */
     public function up()
     {
-      Schema::table('registrations', function (Blueprint $table) {
-        $table->string('eligibility_hsbs')->nullable()->after('centre_id');
-      });
-      Schema::table('registrations', function (Blueprint $table) {
-        $table->string('eligibility_nrpf')->nullable()->after('centre_id');
-      });
-      if (config('app.env') === 'production') {
-        DB::update("UPDATE registrations SET eligibility_hsbs = 'healthy-start-applying'
+        Schema::table('registrations', static function (Blueprint $table) {
+            $table->string('eligibility_hsbs')->nullable()->after('centre_id');
+            $table->string('eligibility_nrpf')->nullable()->after('centre_id');
+        });
+
+        if (config('app.env') === 'production') {
+            DB::update("UPDATE registrations SET eligibility_hsbs = 'healthy-start-applying'
             WHERE eligibility = 'healthy-start-applying'
         ");
-        DB::update("UPDATE registrations SET eligibility_hsbs = 'healthy-start-receiving'
+            DB::update("UPDATE registrations SET eligibility_hsbs = 'healthy-start-receiving'
             WHERE eligibility = 'healthy-start-receiving'
         ");
-        DB::update("UPDATE registrations SET eligibility_hsbs = 'healthy-start-receiving'
+            DB::update("UPDATE registrations SET eligibility_hsbs = 'healthy-start-receiving'
             WHERE eligibility = 'healthy-start'
         ");
-        DB::update("UPDATE registrations SET eligibility_hsbs = 'healthy-start-receiving-not-eligible-or-rejected'
+            DB::update("UPDATE registrations SET eligibility_hsbs = 'healthy-start-receiving-not-eligible-or-rejected'
             WHERE eligibility = 'other'
         ");
-        DB::update("UPDATE registrations SET eligibility_hsbs = 'healthy-start-receiving-not-eligible-or-rejected'
+            DB::update("UPDATE registrations SET eligibility_hsbs = 'healthy-start-receiving-not-eligible-or-rejected'
             WHERE eligibility = 'no-recourse-to-public-funds'
         ");
-        DB::update("UPDATE registrations SET eligibility_nrpf = 'Yes'
+            DB::update("UPDATE registrations SET eligibility_nrpf = 'Yes'
             WHERE eligibility = 'no-recourse-to-public-funds'
         ");
-        DB::update("UPDATE registrations SET eligibility_nrpf = 'No'
+            DB::update("UPDATE registrations SET eligibility_nrpf = 'No'
             WHERE eligibility != 'no-recourse-to-public-funds'
         ");
-      }
-      Schema::table('registrations', function (Blueprint $table) {
-        $table->dropColumn('eligibility');
-      });
+        }
+
+        Schema::table('registrations', static function (Blueprint $table) {
+            $table->dropColumn('eligibility');
+        });
     }
 
     /**
@@ -54,28 +54,30 @@ class UpdateRegistrationsTable extends Migration
      */
     public function down()
     {
-      Schema::table('registrations', function (Blueprint $table) {
-        $table->string('eligibility')->nullable()->after('centre_id');
+        Schema::table('registrations', function (Blueprint $table) {
+            $table->string('eligibility')->nullable()->after('centre_id');
+        });
+
         if (config('app.env') === 'production') {
-          DB::update("UPDATE registrations SET eligibility = 'healthy-start-applying'
+            DB::update("UPDATE registrations SET eligibility = 'healthy-start-applying'
               WHERE eligibility_hsbs = 'healthy-start-applying'
           ");
-          DB::update("UPDATE registrations SET eligibility = 'healthy-start-receiving'
+            DB::update("UPDATE registrations SET eligibility = 'healthy-start-receiving'
               WHERE eligibility_hsbs = 'healthy-start-receiving'
           ");
-          DB::update("UPDATE registrations SET eligibility = 'other'
+            DB::update("UPDATE registrations SET eligibility = 'other'
               WHERE eligibility_hsbs = 'healthy-start-receiving-not-eligible-or-rejected'
           ");
-          DB::update("UPDATE registrations SET eligibility = 'no-recourse-to-public-funds'
+            DB::update("UPDATE registrations SET eligibility = 'no-recourse-to-public-funds'
               WHERE eligibility_nrpf = 'Yes'
           ");
         }
-      });
-      Schema::table('registrations', function (Blueprint $table) {
-        $table->dropColumn('eligibility_hsbs');
-      });
-      Schema::table('registrations', function (Blueprint $table) {
-        $table->dropColumn('eligibility_nrpf');
-      });
+
+        Schema::table('registrations', function (Blueprint $table) {
+            $table->dropColumn('eligibility_hsbs');
+        });
+        Schema::table('registrations', function (Blueprint $table) {
+            $table->dropColumn('eligibility_nrpf');
+        });
     }
 }
