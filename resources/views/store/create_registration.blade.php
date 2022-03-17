e40c21fcd1314ced9695dd11e4756b880e06925b
neonleann <93125276+neonleann@users.noreply.github.com>
2022-03-17 15:53:23 +0000

    Fixing an issue with create registration.

--- resources/views/store/create_registration.blade.php
+++ resources/views/store/create_registration.blade.php
@@ -97,11 +97,7 @@
                         <option value=0>Please select</option>
                         @foreach (config('arc.reg_eligibilities_hsbs') as $index => $reg_eligibility)
                             <option value="{{ $reg_eligibility }}"
-                                    @if(
-                                        (empty(old('eligibility-hsbs')) && $index === 0) ||
-                                        old('eligibility-hsbs') === "healthy-start-receiving"
-                                        ) selected="selected"
-                                    @endif
+                                    @if(old('eligibility-hsbs') === $reg_eligibility) selected="selected" @endif
                             >@lang('arc.reg_eligibilities_hsbs.' . $reg_eligibility)
                             </option>
                         @endforeach
@@ -115,11 +111,7 @@
                         <option value=0>Please select</option>
                         @foreach (config('arc.reg_eligibilities_nrpf') as $index => $reg_eligibility)
                             <option value="{{ $reg_eligibility }}"
-                                    @if(
-                                        (empty(old('eligibility-nrpf')) && $index === 0) ||
-                                        old('eligibility-nrpf') === "healthy-start-receiving"
-                                        ) selected="selected"
-                                    @endif
+                                    @if(old('eligibility-nrpf') === $reg_eligibility) selected="selected" @endif
                             >@lang('arc.reg_eligibilities_nrpf.' . $reg_eligibility)
                             </option>
                         @endforeach
