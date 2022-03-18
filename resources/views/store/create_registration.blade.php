7119b7c51e28dbe2fce5e385c98c4c927e76b66f
Charles Strange <charles@neontribe.co.uk>
2022-03-18 15:43:40 +0000

    fix reg

--- resources/views/store/create_registration.blade.php
+++ resources/views/store/create_registration.blade.php
@@ -97,7 +97,11 @@
                         <option value=0>Please select</option>
                         @foreach (config('arc.reg_eligibilities_hsbs') as $index => $reg_eligibility)
                             <option value="{{ $reg_eligibility }}"
-                                    @if(old('eligibility-hsbs') === $reg_eligibility) selected="selected" @endif
+                                @if(
+                                    (empty(old('eligibility-hsbs')) && $index === 0) ||
+                                    old('eligibility-hsbs') === $reg_eligibility
+                                    ) selected="selected"
+                            @endif
                             >@lang('arc.reg_eligibilities_hsbs.' . $reg_eligibility)
                             </option>
                         @endforeach
@@ -111,7 +115,10 @@
                         <option value=0>Please select</option>
                         @foreach (config('arc.reg_eligibilities_nrpf') as $index => $reg_eligibility)
                             <option value="{{ $reg_eligibility }}"
-                                    @if(old('eligibility-nrpf') === $reg_eligibility) selected="selected" @endif
+                                @if(
+                                    (empty(old('eligibility-nrpf')) && $index === 0) ||
+                                    old('eligibility-nrpf') === $reg_eligibility) selected="selected"
+                                @endif
                             >@lang('arc.reg_eligibilities_nrpf.' . $reg_eligibility)
                             </option>
                         @endforeach
