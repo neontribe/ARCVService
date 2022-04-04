1b9f9c22576e51a00f2e8ff4af842e5bebef1b2f
neonleann <93125276+neonleann@users.noreply.github.com>
2022-04-04 13:17:49 +0100

    Some JS.

--- resources/views/service/partials/family_rule.blade.php
+++ resources/views/service/partials/family_rule.blade.php
@@ -7,16 +7,16 @@
     <label for="name" class="block">Name of rule</label>
 </div>
 <div class="dob-input relative">
-    <input type="radio" class="styled-checkbox" id="family_exists_each" name="family_exists" checked>
+    <input type="radio" class="styled-checkbox" value="family_exists_each" name="family_exists" id="family_exists">
     <label for="family_exists_each">Award vouchers per <i>eligible family member</i> if family exists?</label>
 </div>
 <div class="dob-input relative">
-    <input type="radio" class="styled-checkbox" id="family_exists_total" name="family_exists" checked>
+    <input type="radio" class="styled-checkbox" value="family_exists_total" name="family_exists" id="family_exists">
     <label for="family_exists_total">Award vouchers per <i>family</i> if family exists?</label>
 </div>
 <div class="dob-input">
     <input id="num_vouchers" name="num_vouchers" type="number" pattern="[0-9]*" min="0">
-    <label for="num_vouchers" class="block">Number of vouchers</label>
+    <label for="num_vouchers" class="block">Number of vouchers</label><span id='who_gets_vouchers'></span>
 </div>
 <div class="dob-input relative">
     <input type="checkbox" class="styled-checkbox" id="has_warning" name="has_warning" checked>
@@ -32,3 +32,15 @@
 </div>
 <button class="long-button submit" type="submit">Save</button>
 </form>
+
+<script>
+  $('input[name="family_exists"]')
+    .change(function (){
+        var val = $(this).val();
+        if (val === 'family_exists_total') {
+          $('#who_gets_vouchers').text(' per family');
+        } else {
+          $('#who_gets_vouchers').text(' per family member');
+        }
+  });
+</script>
