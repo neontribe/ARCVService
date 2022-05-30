0f4fefda8688a5aab670610833f16cece6f7bc3b
lauragurney <laura.gurney@gmail.com>
2022-05-30 15:48:58 +0100

    Rename pages

--- resources/views/store/create_registration.blade.php
+++ resources/views/store/create_registration.blade.php
@@ -11,22 +11,22 @@
         <div class="content">
             <form action="{{ URL::route("store.registration.store") }}" method="post" class="full-height">
             {!! csrf_field() !!}
-                @include('store.partials.first_columnSP')
-                @include('store.partials.middle_columnSP')
-                @include('store.partials.last_columnSP')
+                @include('store.partials.voucher_collectorsSP')
+                @include('store.partials.householdSP')
+                @include('store.partials.other_infoSP')
             </form>
         </div>
         @else
         <div class="content">
             <form action="{{ URL::route("store.registration.store") }}" method="post" class="full-height">
             {!! csrf_field() !!}
-                @include('store.partials.first_column')
-                @include('store.partials.middle_column')
-                @include('store.partials.last_column')
+                @include('store.partials.voucher_collectors')
+                @include('store.partials.family')
+                @include('store.partials.other_info')
             </form>
         </div>
         @endif
-        
+
 <script src="{{ asset('js/create_registration.js') }}"></script>
 
 @endsection
