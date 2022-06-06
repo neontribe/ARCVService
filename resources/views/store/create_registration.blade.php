21fb31c52b38dd70913ba0a2510857a986adb5c5
Charles Strange <charles@neontribe.co.uk>
2022-06-06 16:46:17 +0100

    reformat create and edit registration pages

--- resources/views/store/create_registration.blade.php
+++ resources/views/store/create_registration.blade.php
@@ -1,34 +1,35 @@
 @extends('store.layouts.service_master')
 
 @section('content')
-@if ($programme !==0)
-    @include('store.partials.navbar', ['headerTitle' => 'New household sign up'])
-    @section('title', 'Add a new Household')
-@else
-    @include('store.partials.navbar', ['headerTitle' => 'New family sign up'])
-    @section('title', 'Add a new Family')
-@endif
+
+    @if ($programme !==0)
+        @include('store.partials.navbar', ['headerTitle' => 'New household sign up'])
+        @section('title', 'Add a new Household')
+    @else
+        @include('store.partials.navbar', ['headerTitle' => 'New family sign up'])
+        @section('title', 'Add a new Family')
+    @endif
 
     @if ($programme !== 0)
         <div class="content">
             <form action="{{ URL::route("store.registration.store") }}" method="post" class="full-height">
-            {!! csrf_field() !!}
+                {!! csrf_field() !!}
                 @include('store.partials.voucher_collectorsSP')
                 @include('store.partials.householdSP')
                 @include('store.partials.other_infoSP')
             </form>
         </div>
-        @else
+    @else
         <div class="content">
             <form action="{{ URL::route("store.registration.store") }}" method="post" class="full-height">
-            {!! csrf_field() !!}
+                {!! csrf_field() !!}
                 @include('store.partials.voucher_collectors')
                 @include('store.partials.family')
                 @include('store.partials.other_info')
             </form>
         </div>
-        @endif
+    @endif
 
-<script src="{{ asset('store/js/create_registration.js') }}"></script>
-
+    <script src="{{ asset('store/js/create_registration.js') }}"></script>
 @endsection
+
