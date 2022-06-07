3219d4f179eb8c00dabd95b0746bde33ab586b17
Charles Strange <charles@neontribe.co.uk>
2022-06-07 14:54:05 +0100

    fix remove row

--- public/store/js/create_registration.js
+++ public/store/js/create_registration.js
@@ -19,31 +19,6 @@ $(document).ready(
             }
         });
 
-        // In the case of failed submission, iterate the children previously submitted
-        $(".js-old-child").each(function (index) {
-            // Grab the data out of the data attributes
-            var dob = $(this).data("dob");
-            var verified = $(this).data("verified");
-
-            // Convert to useful formats - add_child_form partial should have validated these
-            var dateObj = moment(dob, "YYYY-MM", true).format("MMM YYYY");
-            var childKey = Math.random();
-            var displayVerified = verified === 1 ? "checked" : null;
-            var displayMonths = moment().diff(dob, 'months') % 12;
-            var displayYears = moment().diff(dob, 'years');
-
-            // Create and append new style columns
-            var ageColumn = '<td class="age-col">' + displayYears + ' yr, ' + displayMonths + ' mo</td>';
-            var dobColumn = '<td class="dob-col"><input name="children[' + childKey + '][dob]" type="hidden" value="' + dob + '" >' + dateObj + '</td>';
-            var idColumn = '<td class="verified-col relative"><input type="checkbox" class="styled-checkbox inline-dob" name="children[' + childKey + '][verified]" id="child' + childKey + '" ' + displayVerified + ' value="' + verified + '"><label for="child' + childKey + '"><span class="visually-hidden">Toggle ID checked</span></label>' + '</td>';
-            var removeColumn = '<td class="remove-col"><button type="button" class="remove_date_field"><i class="fa fa-minus" aria-hidden="true"></i></button></td>';
-
-            $(this).append(ageColumn);
-            $(this).append(dobColumn);
-            $(this).append(idColumn);
-            $(this).append(removeColumn);
-        });
-
         $(el).on("click", ".remove_field", function (e) {
             e.preventDefault();
             $(this).closest('tr').remove();
@@ -90,4 +65,10 @@ $('#cancel').click(function (e) {
     $('#expandable').addClass('collapsed');
     $('#leaving').removeClass('expanded');
     e.preventDefault();
+});
+
+$("#child_wrapper").on('click', '.remove_date_field', function (e) {
+    e.preventDefault();
+    $(e.target).closest('tr').remove();
+    return false;
 });
--- resources/views/store/partials/householdSP.blade.php
+++ resources/views/store/partials/householdSP.blade.php
@@ -17,9 +17,7 @@
             <thead>
             <tr>
                 <td class="age-col">Age</td>
-                @if ( $verifying )
-                    <td class="verified-col">ID</td>
-                @endif
+                <td class="dob-col"></td>
                 <td class="remove-col"></td>
             </tr>
             </thead>
@@ -27,7 +25,8 @@
             @if(is_array(old('children')) || (!empty(old('children'))))
                 @foreach (old('children') as $old_child )
                     <tr class="js-old-child"
-                        data-dob={{ $old_child['dob'] }} data-verified={{ $old_child['verified'] ?? 0 }}></tr>
+                        data-dob={{ $old_child['dob'] }}
+                    ></tr>
                 @endforeach
             @endif
             </tbody>
@@ -39,12 +38,7 @@
     function addAgeRow(e, dateObj, verified) {
         // setup fields
         // It's a valid date, so manufacture a human-readable string
-        var innerTextDate = dateObj.format("MMM YYYY");
         var valueDate = dateObj.format("YYYY-MM");
-
-        // Organise the ID verification values and display
-        var verifiedValue = verified ? 1 : 0;
-        var displayVerified = verified ? "checked" : null;
 
         // give the kids a random key
         var childKey = Math.random();
@@ -58,14 +52,11 @@
             : '<td class="age-col">P</td>'
         ;
 
-        var dobColumn = '<td class="dob-col"><input name="children[' + childKey + '][dob]" type="hidden" value="' + valueDate + '" >' + innerTextDate + '</td>';
-        var idColumn = (verified)
-            ? '<td class="verified-col relative"><input type="checkbox" class="styled-checkbox inline-dob" name="children[' + childKey + '][verified]" id="child' + childKey + '" ' + displayVerified + ' value="' + verifiedValue + '"><label for="child' + childKey + '"><span class="visually-hidden">Toggle ID checked</span></label>' + '</td>'
-            : '<td class="verified-col relative"></td>';
+        var dobColumn = '<td class="dob-col"><input name="children[' + childKey + '][dob]" type="hidden" value="' + valueDate + '" ></td>';
         var removeColumn = '<td class="remove-col"><button type="button" class="remove_date_field"><i class="fa fa-minus" aria-hidden="true"></i></button></td>';
 
         // add an input
-        $("#child_wrapper").append('<tr>' + ageColumn + dobColumn + idColumn + removeColumn + '</tr>');
+        $("#child_wrapper").append('<tr>' + ageColumn + dobColumn + removeColumn + '</tr>');
 
         // emit event
         $(document).trigger('childRow:updated');
