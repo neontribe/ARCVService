72bb9a2a835101287a6a131d5ac98a57bcb24c48
Charles Strange <charles@neontribe.co.uk>
2022-06-09 15:56:19 +0100

    make sp form consistent with standard form

--- resources/views/store/partials/voucher_collectorsSP.blade.php
+++ resources/views/store/partials/voucher_collectorsSP.blade.php
@@ -5,29 +5,21 @@
         <h2>Voucher collectors</h2>
     </div>
     <div>
-        <label for="carer">Main participant's full name</label>
-        {{-- This will only already be set for existing registrations and not new ones --}}
+        <label for="carer">Main Participant's full name</label>
         @if (isset($pri_carer))
+            {{-- This section should only exist in edit rather than add new record --}}
             <input id="carer" name="pri_carer[{{ $pri_carer->id }}]"
                    class="@if($errors->has('pri_carer'))invalid @endif" type="text"
                    value="{{ $pri_carer->name }}" autocomplete="off"
                    autocorrect="off" spellcheck="false">
-            {{-- If this is a new record do this instead --}}
+
         @else
-            <input id="carer" name="carer" class="@if($errors->has('carer'))invalid @endif" type="text"
-                   autocomplete="off" autocorrect="off" spellcheck="false" value="{{ old('carer') }}">
+            {{-- If this is a new record do this instead --}}
+            <input id="carer" name="pri_carer" class="@if($errors->has('pri_carer'))invalid @endif" type="text"
+                   autocomplete="off" autocorrect="off" spellcheck="false" value="{{ old('pri_carer') }}">
         @endif
     </div>
-
-    <div id="addCarerAgeInput" class="age-input-container">
-        @include('store.partials.ageInput')
-        <button id="add-carer-age" class="link-button link-button-large">
-            <i class="fa fa-plus button-icon" aria-hidden="true"></i>
-            Add Main Participant
-        </button>
-    </div>
-
-    @if ( $errors->has('carer') )
+    @if ( $errors->has('pri_carer') )
         <div class="alert-message error" id="carer-alert">
             <div class="icon-container error">
                 <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
@@ -37,10 +29,29 @@
             </div>
         </div>
     @endif
+
+    <div id="addCarerAgeInput" class="age-input-container">
+        @include('store.partials.ageInput')
+        <button id="add-carer-age" class="link-button link-button-large">
+            <i class="fa fa-plus button-icon" aria-hidden="true"></i>
+            Add Main Participant
+        </button>
+    </div>
+
     <div>
-        <label for="carer_adder_input">Voucher collectors <span>(optional)</span></label>
+        <label for="carer_adder_input">Voucher collectors (optional)</label>
+        <div id="carer_adder" class="small-button-container">
+            <input id="carer_adder_input" name="carer_adder_input" type="text" autocomplete="off" autocorrect="off"
+                   spellcheck="false">
+            <button id="add_collector" class="add-button">
+                <i class="fa fa-plus" aria-hidden="true"></i>
+            </button>
+        </div>
+    </div>
+    <div class="added">
+        <p>You have added:</p>
         <table id="carer_wrapper">
-            {{-- This section should only exist in edit rather than add new record --}}
+            <!-- edit page -->
             @if (isset($sec_carers))
                 @foreach ( $sec_carers as $sec_carer )
                     <tr>
@@ -56,22 +67,11 @@
                     </tr>
                 @endforeach
             @endif
-        </table>
-    </div>
-    <div id="carer_adder" class="small-button-container">
-        <input id="carer_adder_input" name="carer_adder_input" type="text" autocomplete="off" autocorrect="off"
-               spellcheck="false">
-        <button id="add_collector" class="add-button">
-            <i class="fa fa-plus" aria-hidden="true"></i>
-        </button>
-    </div>
-    <div class="added">
-        <label for="carer_wrapper">You have added: </label>
-        <table id="carer_wrapper">
-            @if(is_array(old('carers')) || (!empty(old('carers'))))
-                @foreach (old('carers') as $old_sec_carer )
+            <!-- create and edit pages, bad submit reload -->
+            @if(is_array(old('new_carers')) || (!empty(old('new_carers'))))
+                @foreach (old('new_carers') as $old_new_carer )
                     <tr>
-                        <td><input name="carers[]" type="text" value="{{ $old_sec_carer }}"></td>
+                        <td><input name="new_carers[]" type="text" value="{{ $old_new_carer }}"></td>
                         <td>
                             <button type="button" class="remove_field"><i class="fa fa-minus" aria-hidden="true"></i>
                             </button>
@@ -83,7 +83,7 @@
     </div>
 </div>
 
-@pushonce('bottom:vouchercollectorSP')
+@pushonce('bottom:vouchercollectorsSP')
     <script>
         $("#addCarerAgeInput").ageInput();
     </script>
