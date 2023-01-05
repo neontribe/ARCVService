4d32d0a0517a1fe6f75f10867aae958ebfbb00db
neonleann <93125276+neonleann@users.noreply.github.com>
2023-01-05 14:50:04 +0000

    Dusk and all the tests.

---
+++ tests/Browser/AdminEditWorkersJSTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\AdminLogin;
+use Tests\DuskTestCase;
+
+class AdminEditWorkersJSTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function edit_workers_javascript_is_working()
+    {
+        $adminLogin = new AdminLogin;
+
+        $this->browse(function ($browser) use ($adminLogin) {
+            $browser->visit($adminLogin)
+                    ->visit('/workers/' . $adminLogin->centreUser->id . '/edit')
+                    ->assertSee('Edit a Children\'s Centre Worker')
+                    ->assertSee('Set Neighbours as Alternatives')
+                    ->assertSee($adminLogin->centreUser->centre->name)
+                    ->assertSelected('worker_centre', $adminLogin->centreUser->centre->id)
+                    ->assertPresent('#neighbour-' . $adminLogin->centres[1]->id)
+                    ->assertMissing('#neighbour-' . $adminLogin->other_centres[1]->id)
+                    ->select('worker_centre', $adminLogin->other_centres[1]->id)
+                    ->assertSelected('worker_centre', $adminLogin->other_centres[1]->id)
+                    ->assertMissing('#neighbour-' . $adminLogin->centres[2]->id)
+                    ->assertPresent('#neighbour-' . $adminLogin->other_centres[2]->id)
+                    ->press('Delete worker')
+                    ->assertDialogOpened('Are you sure you want to delete this worker account?')
+                    ;
+        });
+    }
+}
