d60ce245117a4e98196e3779b4894b9461ebf2a7
karalan <kara@neontribe.co.uk>
2023-03-29 15:01:14 +0100

    PaymentController Tests, Payment Page Tests

--- tests/Feature/Service/PaymentsPageTest.php
+++ tests/Feature/Service/PaymentsPageTest.php
@@ -1,16 +1,19 @@
 <?php
 
-namespace Tests\Feature\Store;
+namespace Tests\Feature\Service;
 
+use App\User;
 use Tests\StoreTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\AdminUser;
 use App\Voucher;
 use App\StateToken;
 use App\Trader;
 use URL;
 
-class PaymentPageTest extends StoreTestCase
+class PaymentsPageTest extends StoreTestCase
 {
+    //All these are copied and adapted from old store payment page tests that were at Feature\Store and now defunct.
     use DatabaseMigrations;
 
     protected $voucher;
@@ -18,10 +21,14 @@ class PaymentPageTest extends StoreTestCase
     protected function setUp(): void
     {
         parent::setUp();
+        // Create Admin as this is all in auth now
+        $this->admin_user = factory(AdminUser::class)->create();
 
         // Create a voucher and a trader with some info
+        // And a user otherwise transition rightly breaks integrity constraints
         $this->voucher = factory(Voucher::class)->state('printed')->create();
         $trader = factory(Trader::class)->state('withnullable')->create();
+        $user = factory(User::class)->create();
         $this->voucher->code = 'TEST12345';
         $this->voucher->trader_id = $trader->id;
         $this->voucher->save();
@@ -37,14 +44,16 @@ class PaymentPageTest extends StoreTestCase
 
         $vs = $this->voucher->history->last();
         $vs->stateToken()->associate($stateToken);
+        $vs->user_id = $user->id;
         $vs->save();
     }
 
     /** @test */
     public function itShowsAnErrorWhenPaymentLinkBad()
     {
+        $this->actingAs($this->admin_user, 'admin')
         // A poorly formed uuid should show the page, but with an error message in place of the voucher table
-        $this->visit(URL::route('store.payment-request.show', [ 'paymentUuid' => 'a-made-up-uuid' ]))
+            ->visit(URL::route('admin.payment-request.show', [ 'paymentUuid' => 'a-made-up-uuid' ]))
             ->seeInElement('p[class="content-warning center"]', 'This payment request is invalid, or has expired.')
             ->dontSeeElement('table')
         ;
@@ -54,18 +63,21 @@ class PaymentPageTest extends StoreTestCase
     public function itShowsPaymentRequestDetailsOnlyForValidPaymentUUIDs()
     {
         // A real uuid will display the data table
-        $this->visit(URL::route('store.payment-request.show', [ 'paymentUuid' => 'a-real-uuid' ]))
+        $this->actingAs($this->admin_user, 'admin')
+            ->visit(URL::route('admin.payment-request.show', [ 'paymentUuid' => 'a-real-uuid' ]))
             ->seeElement('table')
-            ->see('Voucher Code')
-            ->see('Status')
-            ->see('Date')
+            ->seeInElement('th', 'Voucher Code')
+            ->seeInElement('th', 'Status')
+            ->seeInElement('th', 'Date')
         ;
 
-        // A made up uuid uuid will not display the data table
-        $this->visit(URL::route('store.payment-request.show', [ 'paymentUuid' => 'a-made-up-uuid' ]))
+        // A made up uuid will not display the data table
+        $this->actingAs($this->admin_user, 'admin')
+            ->visit(URL::route('admin.payment-request.show', [ 'paymentUuid' => 'a-made-up-uuid' ]))
             ->dontseeElement('table')
-            ->dontsee('Voucher Code')
-            ->dontsee('Status')
+            ->dontseeInElement('th', 'Voucher Code')
+            ->dontseeInElement('th', 'Status')
+            ->dontseeInElement('th', 'Date')
             ->see('This payment request is invalid, or has expired.')
         ;
     }
@@ -74,16 +86,18 @@ class PaymentPageTest extends StoreTestCase
     public function itShowsPayButtonWhenOnlyPaymentIsUnpaid()
     {
         // A real unpaid uuid will display the pay button
-        $this->visit(URL::route('store.payment-request.show', [ 'paymentUuid' => 'a-real-uuid' ]))
-            ->seeInElement('button[type="submit"]', 'Pay')
+        $this->actingAs($this->admin_user, 'admin')
+            ->visit(URL::route('admin.payment-request.show', [ 'paymentUuid' => 'a-real-uuid' ]))
+            ->seeInElement('button[class="btn link-button"]', 'Pay <b>1</b> Vouchers')
         ;
 
         // Transition the voucher to paid
         $this->voucher->applyTransition('payout');
 
         // A real paid uuid will not display the pay button
-        $this->visit(URL::route('store.payment-request.show', [ 'paymentUuid' => 'a-real-uuid' ]))
-            ->dontSeeInElement('button[type="submit"]', 'Pay')
+        $this->actingAs($this->admin_user, 'admin')
+            ->visit(URL::route('admin.payment-request.show', [ 'paymentUuid' => 'a-real-uuid' ]))
+            ->dontSeeInElement('button[class="btn link-button"]', 'Pay <b>1</b> Vouchers')
         ;
     }
 
@@ -91,23 +105,27 @@ class PaymentPageTest extends StoreTestCase
     public function itShowsTheCorrectVoucherStatus()
     {
         // A made up uuid will display no statuses
-        $this->visit(URL::route('store.payment-request.show', [ 'paymentUuid' => 'a-made-up-uuid' ]))
+        $this->actingAs($this->admin_user, 'admin')
+            ->visit(URL::route('admin.payment-request.show', [ 'paymentUuid' => 'a-made-up-uuid' ]))
             ->dontSeeInElement('span[class="status requested"]', 'Requested')
             ->dontSeeInElement('span[class="status paid"]', 'Paid')
         ;
 
         // A real unpaid uuid will display requested status
-        $this->visit(URL::route('store.payment-request.show', [ 'paymentUuid' => 'a-real-uuid' ]))
-            ->seeInElement('span[class="status requested"]', 'Requested')
+        $this->actingAs($this->admin_user, 'admin')
+            ->visit(URL::route('admin.payment-request.show', [ 'paymentUuid' => 'a-real-uuid' ]))
+            ->seeInElement('span[class="link-button link-button-small requested"]', 'Requested')
         ;
 
         // Transition the voucher to paid
         $this->voucher->applyTransition('payout');
 
         // A real paid uuid will display paid status
-        $this->visit(URL::route('store.payment-request.show', [ 'paymentUuid' => 'a-real-uuid' ]))
+        $this->actingAs($this->admin_user, 'admin')
+            ->visit(URL::route('admin.payment-request.show', [ 'paymentUuid' => 'a-real-uuid' ]))
             ->seeInElement('span[class="status paid"]', 'Paid')
         ;
     }
 
 }
+
--- tests/Unit/Controllers/Service/Admin/PaymentControllerTest.php
+++ tests/Unit/Controllers/Service/Admin/PaymentControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Controllers\Service\Admin;
 
 use App\AdminUser;
+use App\Http\Controllers\Service\Admin\PaymentsController;
 use App\StateToken;
 use App\Trader;
 use App\VoucherState;
@@ -42,7 +43,7 @@ class PaymentControllerTest extends StoreTestCase
 
     {
         //Create a token to pass to the route
-        //Create some vouchers, give them a sponsor (as otherwise it migght error)
+        //Create some vouchers, give them a sponsor (as otherwise it might error)
         // and progress them to payment_pending to get a UUID
 
         $token = factory(StateToken::class)->create();
@@ -78,21 +79,47 @@ class PaymentControllerTest extends StoreTestCase
         }
         //TODO also test that I cannot see a different UUID in here
     }
-//    /** @test */
-//    public function testItUpdatesASpecificPaymentRequest()
-//
-//
-//    {
-//        //make a set of vouchers that are payment pending
-//
-//        //give them a Uuid
-//
-//        //check the voucher states
-//
-//        //push them through the transistion
-//
-//        //check the states again
-//
-//    }
+    /** @test */
+    public function testItUpdatesASpecificPaymentRequest()
 
+    {
+        //Create a token to pass to the route
+        //Create some vouchers, give them a sponsor (as otherwise it might error)
+        // and progress them to payment_pending to get a UUID
+
+        $token = factory(StateToken::class)->create();
+        $s = factory(Sponsor::class)->create();
+        $u = factory(User::class)->create();
+        // Make a pile of vouchers that are payment_pending
+        $this->vouchers = factory(Voucher::class, 5)->state('printed')->create();
+        foreach ($this->vouchers as $k => $voucher) {
+            $voucher->code = 'RVNT' . str_pad($k, 4, '0', STR_PAD_LEFT);
+            $voucher->sponsor_id = $s->id;
+            $voucher->trader_id = $this->trader->id;
+            // Progress to dispatched.
+            $voucher->applyTransition('dispatch');
+            $voucher->applyTransition('collect');
+            $voucher->applyTransition('confirm');
+
+            $voucherState = $voucher->paymentPendedOn()->first();
+            $voucherState->state_token_id = $token->id;
+            $voucherState->voucher_id = $voucher->id;
+            $voucherState->user_id = $u->id;
+            $voucherState->save();
+            $voucher->save();
+        }
+
+        $data = $token->uuid;
+
+        //pass the UUID to the route
+        $route = route('admin.payment-request.update', ['paymentUuid' => $data]);
+
+        $this->actingAs($this->admin_user, 'admin')
+            ->put($route)
+            ->followRedirects()
+            ->assertResponseStatus(200)
+            ->seePageIs(route('admin.payments.index'))
+            ->see('Vouchers Paid!');
+
+    }
 }
