dc5a554e94c0706f9a7605f70e4ee76e25602744
Toby Batch <tobias@neontribe.co.uk>
2023-06-22 09:59:00 +0100

    fix: remove http links from the templates (#663)

--- resources/views/api/emails/voucher_payrequest_email_textonly.blade.php
+++ resources/views/api/emails/voucher_payrequest_email_textonly.blade.php
@@ -24,7 +24,7 @@ Rose Vouchers
 
 Alexandra Rose Charity
 
-For more information please go to <a href="http://www.alexandrarose.org.uk/" target="_blank" rel="noopener noreferrer">www.alexandrarose.org.uk</a>.
+For more information please go to <a href="https://www.alexandrarose.org.uk/" target="_blank" rel="noopener noreferrer">www.alexandrarose.org.uk</a>.
 
 <a href="https://www.alexandrarose.org.uk/Handlers/Download.ashx?IDMF=0a18fbf9-ed33-4c1d-a8a3-766542c961c8" target="_blank" rel="noopener noreferrer">
     Privacy Policy
--- resources/views/store/layouts/email.blade.php
+++ resources/views/store/layouts/email.blade.php
@@ -17,7 +17,7 @@
 
         <div class="footer" style="margin-top: 40px; font-size: 13px; color: #999;">
             <p>&copy; Alexandra Rose Charity</p>
-            <p>For more information please go to <a href="http://www.alexandrarose.org.uk/" role="link" target="_blank" rel="noopener noreferrer">www.alexandrarose.org.uk</a>.</p>
+            <p>For more information please go to <a href="https://www.alexandrarose.org.uk/" role="link" target="_blank" rel="noopener noreferrer">www.alexandrarose.org.uk</a>.</p>
             <p><a href="{{ config('arc.links.privacy_policy') }}" role="link" target="_blank" rel="noopener noreferrer">Privacy Policy</a></p>
         </div>
 
