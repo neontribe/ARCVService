6c62a8219e0fe7e5e4e70c97c582ec589d7d1dec
Tobias Batch <tobias@neontribe.co.uk>
2023-06-28 07:47:27 +0100

    feat: cleaned out docker stuff, I'm moving it to ARCVInfra

--- .docker/yarn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# docker compose exec arc yarn does not run the .bashrc
-# we could use a forced tty login, e.g. docker compose exec arc bash --login yarn
-# we'll just wrap that up here
-
-source ~/.bashrc
-yarn $@
