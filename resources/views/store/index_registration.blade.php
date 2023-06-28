a19bf233fe6d8da7381da4e3be186f94a1ffa86e
Toby Batch <tobias@neontribe.co.uk>
2023-06-28 15:58:21 +0100

    1.16/2051 fuzzy search issues with displayed results vvv eee (#674)
    * chore: more investigation about fuzzy searching
    
    * feat: adds ui for toggle fuzzy
    
    * feat: refined the ui for toggle fuzzy search
    
    * feat: fuzzy backend

--- resources/views/store/index_registration.blade.php
+++ resources/views/store/index_registration.blade.php
@@ -12,7 +12,7 @@
                 {!! csrf_field() !!}
                 {{-- Families left checkbox --}}
                 <div class="checkbox-control">
-                    <input type="checkbox" class="styled-checkbox no-margin" onChange="this.form.submit()" id="families_left" name="families_left" {{
+                    <input type="checkbox" class="styled-checkbox no-margin" onChange="submitSearchForm()" id="families_left" name="families_left" {{
                     Request::get("families_left") ? 'checked' : '' }} />
                     <label for="families_left">Show {{ $programme ? 'households' : 'families'}} who have left</label>
                 </div>
@@ -24,6 +24,33 @@
                                spellcheck="false" onkeyup="searchForm()"
                                placeholder="Enter {{ $programme ? 'household' : 'family'}} name" aria-label="{{ $programme ? 'Household' : 'Family'}} Name"
                                value="{{ Request::get("family_name") ?? '' }}" />
+
+                        <input type="hidden" name="fuzzy" value="{{ $fuzzy }}">
+                        <div class="fuzzy-search">
+                            <button class="btn" onClick="toggleFuzzySearch(); return false;">
+                                <i id="fuzzy-search-icon"
+                                   @if ($fuzzy)
+                                        class="fa fa-dot-circle-o fuzzy-on"
+                                   @else
+                                        class="fa fa-dot-circle-o"
+                                   @endif
+                                   aria-hidden="true"></i>
+                            </button>
+                            <div class="fuzzy-search-content" id="fuzzy-search-content">
+                                <a href="#"
+                                   id="fuzzy-search-exact"
+                                   @if (! $fuzzy)
+                                        class="fuzzy-text-on"
+                                   @endif
+                                   onClick="setExactSearch()">Exact</a>
+                                <a href="#"
+                                   id="fuzzy-search-fuzzy"
+                                   @if ($fuzzy)
+                                        class="fuzzy-text-on"
+                                   @endif
+                                   onClick="setFuzzySearch()">Fuzzy</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </form>
@@ -97,7 +124,7 @@
         function searchForm() {
             // setup before functions
             var typingTimer;                //timer identifier
-            var doneTypingInterval = 500;   //time in ms (half a second)
+            var doneTypingInterval = 750;   //time in ms (half a second)
             // on keyup, start the countdown
             $("#family_name").keyup(function(){
                 clearTimeout(typingTimer);
@@ -108,6 +135,34 @@
             // user is "finished typing" do something
             function doneTyping () {
                 $("#searchform").submit();
+            }
+        }
+
+        function toggleFuzzySearch() {
+            if ($("input[name='fuzzy']").val() === "0") {
+                setFuzzySearch();
+            } else {
+                setExactSearch();
+            }
+        }
+
+        function setExactSearch() {
+            $("input[name='fuzzy']").val(0);
+            document.getElementById("fuzzy-search-exact").classList.add("fuzzy-text-on");
+            document.getElementById("fuzzy-search-fuzzy").classList.remove("fuzzy-text-on");
+            document.getElementById("fuzzy-search-icon").classList.remove("fuzzy-on");
+        }
+
+        function setFuzzySearch() {
+            $("input[name='fuzzy']").val(1);
+            document.getElementById("fuzzy-search-exact").classList.remove("fuzzy-text-on");
+            document.getElementById("fuzzy-search-fuzzy").classList.add("fuzzy-text-on");
+            document.getElementById("fuzzy-search-icon").classList.add("fuzzy-on");
+        }
+
+        function submitSearchForm() {
+            if (document.getElementById("family_name").val().length >= 3) {
+                document.getElementById("searchform").submit();
             }
         }
     </script>
