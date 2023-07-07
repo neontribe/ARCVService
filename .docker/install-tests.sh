b276590a95732e3603ef17689cb0aa4d0ddf45ea
Toby Batch <tobias@neontribe.co.uk>
2023-07-07 12:22:51 +0100

    feat: cleaned out docker stuff, I'm moving it to ARCVInfra (#672) (#676)
    * feat: cleaned out docker stuff, I'm moving it to ARCVInfra
    
    * docs: tidy readme

--- .docker/install-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get -y install -y libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 chromium xvfb gtk2-engines-pixbuf xfonts-cyrillic xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable imagemagick x11-apps
-
-Xvfb -ac :0 -screen 0 1280x1024x16 &
-chmod a+x ./vendor/laravel/dusk/bin/chromedriver-linux
-./vendor/laravel/dusk/bin/chromedriver-linux --port=8888 &
-php artisan serve &
