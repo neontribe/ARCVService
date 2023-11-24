7bcedc737e34633570278019cb9922a75c094769
Gabriel Lancaster-West <116901054+gxnda@users.noreply.github.com>
2023-11-24 15:01:31 +0000

    Write encrypted logs to file rather than database (#727)
    * feat: added ui for log downloads by f-year
    
    * feat: Encrypts and Decrypts MVL files.
    
    * chore: removed and improved some comments
    
    * feat: Logs now export as encrypted CSV files, instead of to a database.
    
    * chore: Added more comments.
    
    * chore: Removed unused imports.
    
    * BREAKING CHANGE: Encrypted market logs are now saved as '.arcx.csv' (was '.csv')

--- app/Http/Controllers/API/LoggingController.php
+++ app/Http/Controllers/API/LoggingController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Wrappers\SecretStreamWrapper; // Not sure if this is used? It might be used when declaring ssw:// in log().
 use App\Http\Controllers\Controller;
-use App\MarketLog;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -14,8 +14,6 @@ class LoggingController extends Controller
 
     public function log(Request $request): JsonResponse
     {
-        // Doesn't work, why not?
-        // $json = $request->json();
 
         try {
             $json = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
@@ -24,21 +22,50 @@ class LoggingController extends Controller
             return response()->json([]);
         }
 
+        // Rewriting JSON as CSV to storage/logs/market_logs.csv
+        // TODO: Test this.
+        // TODO: Add method to decrypt the CSV.
         $processed = [];
         if (!empty($json)) {
-            foreach ($json as $hash => $item) {
-                // write to DB
-                $marketLog = new MarketLog();
-                $marketLog->hash = $hash;
-                $marketLog->url = $item['config']['url'] ?? '';
-                $marketLog->status = $item['status'] ?? -1;
-                $marketLog->created = $item['created'] ?? '' ;
-                $marketLog->data = json_encode($item);
-                $marketLog->trader_id = $item['trader'] ?? -1;
-                $marketLog->save();
-                $processed[] = $hash;
+
+            $headers = ["hash", "url", "status", "created", "data","trader-id"];
+            $csvFilePath = "logs/market_logs.arcx.csv";
+
+            // Add encryption wrapper
+            if (!in_array("ssw", stream_get_wrappers())) {
+                stream_wrapper_register("ssw", "App\Wrappers\SecretStreamWrapper");
             }
+
+            // Opens the file using the ssw:// (SecretSteamWrapper) wrapper,
+            // so anything written to $csvFile is encrypted.
+            $csvFile = fopen("ssw://" . $csvFilePath, "a");
+
+            if (!$csvFile) {
+                Log::error("Unable to open CSV file.");
+                return response()->json();
+            }
+
+            // Add headers to the CSV if its empty, prevents headers being appended to existing CSV files.
+            $csvIsEmpty = (filesize($csvFilePath) === 0);
+            if ($csvIsEmpty) {
+                fputcsv($csvFile, $headers);
+            }
+
+            foreach ($json as $hash => $item) {
+                $url = $item['config']['url'] ?? '';
+                $status = $item['status'] ?? -1;
+                $created = $item['created'] ?? '' ;
+                $jsonData = json_encode($item);
+                $trader_id = $item['trader'] ?? -1;
+                $processed[] = $hash;
+
+                // Write data to CSV - this is encrypted because $csvFile is opened using ssw://.
+                fputcsv($csvFile, [$hash, $url, $status, $created, $jsonData, $trader_id]);
+            }
+
+            stream_wrapper_unregister("ssw");
         }
+
         return response()->json($processed);
     }
 }
