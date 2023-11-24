8c36442cd1a1d0a2365c079c1acaaacce1b521bb
Gabriel Lancaster-West <gabriellancasterwest@gmail.com>
2023-11-24 12:42:38 +0000

    feat: Logs now export as encrypted CSV files, instead of to a database.

--- app/Http/Controllers/API/LoggingController.php
+++ app/Http/Controllers/API/LoggingController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Wrappers\SecretStreamWrapper;
 use App\Http\Controllers\Controller;
 use App\MarketLog;
+use False\True;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -24,21 +26,46 @@ class LoggingController extends Controller
             return response()->json([]);
         }
 
+        // Rewriting JSON as CSV to storage/logs/market_logs.csv
+        // TODO: Test this.
+        // TODO: Add method to decrypt the CSV.
         $processed = [];
         if (!empty($json)) {
-            foreach ($json as $hash => $item) {
-                // write to DB
-                $marketLog = new MarketLog();
-                $marketLog->hash = $hash;
-                $marketLog->url = $item['config']['url'] ?? '';
-                $marketLog->status = $item['status'] ?? -1;
-                $marketLog->created = $item['created'] ?? '' ;
-                $marketLog->data = json_encode($item);
-                $marketLog->trader_id = $item['trader'] ?? -1;
-                $marketLog->save();
-                $processed[] = $hash;
+
+            $headers = ["hash", "url", "status", "created", "data","trader-id"];
+            $csvFilePath = "logs/market_logs.csv";
+
+            // Add encryption wrapper
+            if (!in_array("ssw", stream_get_wrappers())) {
+                stream_wrapper_register("ssw", "App\Wrappers\SecretStreamWrapper");
             }
+
+            $csvFile = fopen("ssw://" . $csvFilePath, "a");
+
+            if (!$csvFile) {
+                Log::error("Unable to open CSV file.");
+                return response()->json();
+            }
+
+            $csvIsEmpty = (filesize($csvFilePath) === 0);
+            if ($csvIsEmpty) {
+                fputcsv($csvFile, $headers);
+            }
+
+            foreach ($json as $hash => $item) {
+                $url = $item['config']['url'] ?? '';
+                $status = $item['status'] ?? -1;
+                $created = $item['created'] ?? '' ;
+                $jsonData = json_encode($item);
+                $trader_id = $item['trader'] ?? -1;
+                $processed[] = $hash;
+
+                fputcsv($csvFile, [$hash, $url, $status, $created, $jsonData, $trader_id]);
+            }
+
+            stream_wrapper_unregister("ssw");
         }
+
         return response()->json($processed);
     }
 }
