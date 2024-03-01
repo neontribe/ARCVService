917dfde3fbf1165ffc3205bbc8b40f199192142e
lauragurney <laura.gurney@gmail.com>
2024-03-01 13:49:46 +0000

    fix: larastan on Carer.php

--- app/Carer.php
+++ app/Carer.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\Traits\Aliasable;
+use Eloquent;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
