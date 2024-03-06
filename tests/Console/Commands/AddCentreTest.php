60dfb234777c78f7b194a4b500db0ff74249f8c1
Tobias Batch <tobias@neontribe.co.uk>
2024-03-06 15:38:03 +0000

    fix: coverage for add centre

--- tests/Console/Commands/AddCentreTest.php
+++ tests/Console/Commands/AddCentreTest.php
@@ -8,11 +8,13 @@ use App\Sponsor;
 use Artisan;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase;
 
 class AddCentreTest extends TestCase
 {
     use DatabaseMigrations;
+    use RefreshDatabase;
 
     public function createApplication(): Application
     {
@@ -23,17 +25,30 @@ class AddCentreTest extends TestCase
 
     public function testCommand()
     {
-        $centreUser = CentreUser::first();
-        $sponsor = Sponsor::first();
-        $centre = Centre::first();
+        $count = Sponsor::all()->count();
+        print("Sponsors count = " . $count . "\n");
+
+        $sponsor = new Sponsor();
+        $sponsor->name = "Mr Bolt";
+        $sponsor->shortcode = "TR";
+        $sponsor->save();
+
+        $centreUser = new CentreUser();
+        $centreUser->name = "Barney McGrew";
+        $centreUser->email = "b.mcgrew@example.com";
+        $centreUser->password = "password";
+        $centreUser->role = "foo";
+        $centreUser->save();
+
         $args = sprintf(
             "%s %s %s %s %s",
-            "Trumpton",
-            "NA",
-            $sponsor->shortcode,
-            "individual",
-            $centreUser->email,
+            "Trumpton", # name
+            "NA", # prefix
+            "TR", # shortcode
+            "individual", # pref
+            $centreUser->email # email
         );
+        $cli = "arc:addCentre " . $args;
         $this->withoutMockingConsoleOutput()->artisan("arc:addCentre " . $args);
         $result = Artisan::output();
         $this->assertEquals(
