06c164a63675cdea3925000b7bf3e4638412a480
Toby Batch <tobias@neontribe.co.uk>
2024-04-03 15:59:22 +0100

    Update export-all-in-years.sh

--- bin/export-all-in-years.sh
+++ bin/export-all-in-years.sh
@@ -6,3 +6,4 @@ $ARTISAN arc:mvl:export --chunk-size=999950 --from=01/04/2019 --to=30/03/2020
 $ARTISAN arc:mvl:export --chunk-size=999950 --from=01/04/2020 --to=30/03/2021
 $ARTISAN arc:mvl:export --chunk-size=999950 --from=01/04/2021 --to=30/03/2022
 $ARTISAN arc:mvl:export --chunk-size=999950 --from=01/04/2022 --to=30/03/2023
+$ARTISAN arc:mvl:export --chunk-size=999950 --from=01/04/2023 --to=30/03/2024
