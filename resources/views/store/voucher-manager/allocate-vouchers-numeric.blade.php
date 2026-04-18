fa52b61aa241b6e4bd5e2ed527f85f4450d05db1
charles strange <charles@neontribe.co.uk>
2026-04-19 00:39:50 +0100

    refactor: create allocate by numbers blade

---
+++ resources/views/store/voucher-manager/allocate-vouchers-numeric.blade.php
@@ -0,0 +1,110 @@
+{{-- Requires: $registration, $vouchers, $vouchers_amount, $entitlement, $errors --}}
+<div class="col allocation">
+    <div>
+        <img src="{{ asset('store/assets/allocation-light.svg') }}">
+        <h2 id="allocate-vouchers">Allocate Vouchers</h2>
+    </div>
+
+    {{-- Quantity entry --}}
+    <form method="POST" action="{{ route('store.registration.vouchers.post', ['registration' => $registration->id]) }}">
+        @csrf
+        <div class="single-container">
+            <label for="voucher-quantity">Number of vouchers
+                <input
+                    id="voucher-quantity"
+                    name="voucher-quantity"
+                    type="number"
+                    min="1"
+                    max="{{ config('arc.bundle_max_voucher_append') }}"
+                    value="{{ $entitlement }}"
+                    autocomplete="off"
+                >
+            </label>
+            <button id="quantity-add" class="add-button" type="submit" name="quantity-add">
+                <i class="fa fa-plus" aria-hidden="true"></i>
+            </button>
+        </div>
+    </form>
+
+    @includeWhen($errors->count() > 0, 'store.partials.errors', ['error_array' => $errors->all()])
+    @includeWhen(Session::get('error_messages'), 'store.partials.errors', ['error_array' => Session::get('error_messages')])
+
+    <button id="collection-button" class="long-button" @disabled($vouchers_amount == 0)>
+        <i class="fa fa-ticket button-icon" aria-hidden="true"></i>Go to voucher collection
+    </button>
+
+    <div class="center" id="vouchers-added">
+        <span class="emphasised-section">Vouchers added</span>
+        <span class="number-circle">{{ $vouchers_amount }}</span>
+
+        <div @class(['collapsed' => $vouchers_amount === 0])>
+            <form
+                id="unbundle-all"
+                name="unbundle-all"
+                method="POST"
+                action="{{ route('store.registration.vouchers.delete', ['registration' => $registration->id]) }}"
+                class="delete-button remove-all-container"
+            >
+                @method('DELETE')
+                @csrf
+                <button type="submit" name="delete-all-button" id="delete-all-button">
+                    <i class="fa fa-trash" aria-hidden="true"></i> Remove all added vouchers
+                </button>
+            </form>
+        </div>
+
+        <div id="vouchers" @class(['collapsed' => $vouchers_amount === 0])>
+            <form id="unbundle" name="unbundle" method="POST">
+                @method('DELETE')
+                @csrf
+                <table>
+                    <tr>
+                        <th>Voucher code</th>
+                        <th>Remove</th>
+                    </tr>
+                    @foreach($vouchers as $voucher)
+                        <tr>
+                            <td>{{ $voucher->code }}</td>
+                            <td>
+                                <button
+                                    type="submit"
+                                    class="delete-button"
+                                    name="delete-button"
+                                    id="{{ $voucher->id }}"
+                                    formaction="{{ route('store.registration.voucher.delete', ['registration' => $registration->id, 'voucher' => $voucher->id]) }}"
+                                >
+                                    <i class="fa fa-minus" aria-hidden="true"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </form>
+        </div>
+    </div>
+</div>
+
+@pushonce('js')
+    <script>
+        $(document).ready(function () {
+            $('#collection-button').click(function (e) {
+                e.preventDefault();
+                $('#collection').addClass('slide-in');
+                $('.allocation').addClass('fade-back');
+            });
+
+            if ($('#vouchers tr').length > 1) { // first tr is the header
+                $('#vouchers-added').addClass('pulse');
+            }
+
+            $('#voucher-quantity').keypress(function (e) {
+                if (e.keyCode !== 13) return;
+                e.preventDefault();
+                window.setTimeout(function () {
+                    $('#quantity-add').trigger('click');
+                }, 200);
+            });
+        });
+    </script>
+@endpushonce
+
