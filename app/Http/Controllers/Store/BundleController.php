bb214a6ec76de4c5f370c94081c5785ac33e18c8
charles strange <charles@neontribe.co.uk>
2026-04-29 15:50:26 +0100

    fix: the voucher pool belongs to the centre

--- app/Http/Controllers/Store/BundleController.php
+++ app/Http/Controllers/Store/BundleController.php
@@ -75,7 +75,9 @@ class BundleController extends Controller
 
         if ($request->filled('voucher-quantity')) {
             try {
-                $voucherCodes = Voucher::claimFromPool((int)$request->input('voucher-quantity'))
+                $voucherCodes = $registration->centre->claimFromPool(
+                    (int)$request->input('voucher-quantity'),
+                )
                     ->pluck('code')
                     ->all();
             } catch (Throwable $e) {
